d7e9386a2863970eeea18bbc0ac66fb117d6cdc8
Mick Jordan <mick.jordan@oracle.com>
2014-02-20 18:13:19 -0800

    basic RFFI APIs for builtins and JNR impl

---
+++ com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/ffi/RFFIFactory.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014, 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.r.runtime.ffi;
+
+import com.oracle.truffle.r.runtime.*;
+
+/**
+ * Factory class for the different possible implementations of the {@loink RFFI} interface.
+ * Specification is based on system property {@value #FACTORY_CLASS_PROPERTY}. Current default is a
+ * (naive) JNR-based implementation.
+ */
+public abstract class RFFIFactory {
+    public class RFFIException extends Exception {
+        private static final long serialVersionUID = -5689472664825730485L;
+
+        public RFFIException(String msg, Throwable ex) {
+            super(msg, ex);
+        }
+    }
+
+    private static final String FACTORY_CLASS_PROPERTY = "fastr.ffi.factory.class";
+    private static final String DEFAULT_FACTORY_CLASS = "com.oracle.truffle.r.runtime.ffi.jnr.JNR_RFFIFactory";
+
+    static {
+        final String prop = System.getProperty(FACTORY_CLASS_PROPERTY, DEFAULT_FACTORY_CLASS);
+        try {
+            theFactory = (RFFIFactory) Class.forName(prop).newInstance();
+        } catch (Exception ex) {
+            Utils.fail("Failed to instantiate class: " + prop);
+        }
+    }
+
+    protected static RFFIFactory theFactory;
+    protected static final RFFI theFFI = theFactory.createRFFI();
+
+    protected static RFFIFactory getFactory() {
+        return theFactory;
+    }
+
+    public static RFFI getRFFI() {
+        return theFFI;
+    }
+
+    /**
+     * Subclass implements this method to actually create the concrete {@link RFFI} instance.
+     */
+    protected abstract RFFI createRFFI();
+
+}
---
+++ com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/ffi/jnr/JNR_RFFIFactory.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2014, 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.r.runtime.ffi.jnr;
+
+import java.lang.reflect.*;
+import java.util.*;
+
+import jnr.ffi.*;
+import jnr.ffi.annotations.*;
+import jnr.posix.*;
+import jnr.posix.util.*;
+
+import com.oracle.truffle.r.runtime.ffi.*;
+
+/**
+ * A naive JNR-based factory that supports access to POSIX functions only. Naive because it uses
+ * reflection to invoke the (generic) target method. Access to the base functions is as efficient as
+ * it can be with JNR.
+ */
+public class JNR_RFFIFactory extends BaseRFFIFactory {
+    protected static final POSIX posix = POSIXFactory.getPOSIX(new DefaultPOSIXHandler(), true);
+    private static final Map<String, Method> posixFunctions = new HashMap<>();
+
+    static {
+        for (Method m : POSIX.class.getDeclaredMethods()) {
+            posixFunctions.put(m.getName(), m);
+        }
+    }
+
+    @Override
+    protected RFFI createRFFI() {
+        return this;
+    }
+
+    public Object invoke(Object handle, Object[] args) throws RFFIException {
+        Method m = posixFunctions.get(handle);
+        if (m == null) {
+            throw new RFFIException("JNR function " + ((String) handle) + " not found", null);
+        }
+        try {
+            return m.invoke(posix, args);
+        } catch (InvocationTargetException | IllegalAccessException ex) {
+            throw new RFFIException("JNR invoke exception", ex);
+        }
+    }
+
+    public int getpid() {
+        return posix.getpid();
+    }
+
+    public int setwd(String dir) {
+        return posix.chdir(dir);
+    }
+
+    public interface LibCX {
+        int getcwd(@Out byte[] path);
+    }
+
+    private static LibCX libcx;
+
+    public String getwd() {
+        // For some reason the JNR POSIX interface does not include getcwd
+        if (libcx == null) {
+            libcx = LibraryLoader.create(LibCX.class).load("c");
+        }
+        byte[] buf = new byte[4096];
+        int rc = libcx.getcwd(buf);
+        if (rc == 0) {
+            return null;
+        } else {
+            int i = 0;
+            while (buf[i] != 0 && i < buf.length) {
+                i++;
+            }
+            return new String(buf, 0, i);
+        }
+    }
+
+    public Object getHandle(String name) {
+        return name;
+    }
+
+}
