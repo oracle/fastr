35e79fdd0017592eb7c1a6948131504ba7ac2a79
Adam Welc <adam.welc@oracle.com>
2014-06-04 07:12:10 +0200

    Specialized implementation of "crossprod" when operating on a single double matrix.

--- com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/builtin/base/Crossprod.java
+++ com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/builtin/base/Crossprod.java
@@ -24,30 +24,88 @@ package com.oracle.truffle.r.nodes.builtin.base;
 
 import static com.oracle.truffle.r.nodes.builtin.RBuiltinKind.*;
 
+import com.oracle.truffle.api.*;
 import com.oracle.truffle.api.dsl.*;
 import com.oracle.truffle.api.frame.*;
 import com.oracle.truffle.r.nodes.*;
 import com.oracle.truffle.r.nodes.builtin.*;
 import com.oracle.truffle.r.runtime.data.*;
 import com.oracle.truffle.r.runtime.data.model.*;
+import com.oracle.truffle.r.runtime.ops.na.*;
 
 @RBuiltin(name = "crossprod", kind = INTERNAL)
 public abstract class Crossprod extends RBuiltinNode {
 
     // TODO: this is supposed to be slightly faster than t(x) %*% y but for now it should suffice
 
-    @Child protected MatMult matMult = MatMultFactory.create(new RNode[1], getBuiltin());
-    @Child protected Transpose transpose = TransposeFactory.create(new RNode[1], getBuiltin());
+    @Child MatMult matMult;
+    @Child Transpose transpose;
+    protected final NACheck na = NACheck.create();
 
-    public Object crossprod(VirtualFrame frame, RAbstractVector vector1, RAbstractVector vector2) {
-        controlVisibility();
-        return matMult.executeObject(frame, transpose.execute(frame, vector1), vector2);
+    private Object matMult(VirtualFrame frame, Object op1, Object op2) {
+        if (matMult == null) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            matMult = insert(MatMultFactory.create(new RNode[1], getBuiltin()));
+        }
+        return matMult.executeObject(frame, op1, op2);
+    }
+
+    private Object transpose(VirtualFrame frame, Object value) {
+        if (transpose == null) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            transpose = insert(TransposeFactory.create(new RNode[1], getBuiltin()));
+        }
+        return transpose.execute(frame, value);
     }
 
     @Specialization
-    @SuppressWarnings("unused")
-    public Object dimWithDimensions(VirtualFrame frame, RAbstractVector vector1, RNull vector2) {
+    public Object crossprod(VirtualFrame frame, RAbstractVector a, RAbstractVector b) {
         controlVisibility();
-        return matMult.executeObject(frame, transpose.execute(frame, vector1), vector1);
+        return matMult(frame, transpose(frame, a), b);
     }
+
+    @Specialization(order = 1, guards = "!matdouble")
+    public Object crossprod(VirtualFrame frame, RAbstractVector b, @SuppressWarnings("unused") RNull a) {
+        controlVisibility();
+        return matMult(frame, transpose(frame, b), b);
+    }
+
+    @Specialization(order = 2, guards = "matdouble")
+    public Object crossprodDoubleMatrix(RAbstractDoubleVector a, @SuppressWarnings("unused") RNull b) {
+        controlVisibility();
+        final int aCols = a.getDimensions()[1];
+        final int bRows = a.getDimensions()[0];
+        assert aCols == bRows;
+        final int aRows = aCols;
+        final int bCols = bRows;
+        double[] result = new double[aRows * bCols];
+        na.enable(a);
+        for (int row = 0; row < aRows; ++row) {
+            for (int col = 0; col < bCols; ++col) {
+                double x = 0.0;
+                for (int k = 0; k < aCols; ++k) {
+                    double op1 = a.getDataAt(row * bRows + k);
+                    double op2 = a.getDataAt(col * bRows + k);
+                    if (na.checkNAorNaN(op1)) {
+                        x = op1;
+                        break;
+                    } else if (na.check(op2)) {
+                        x = op2;
+                        break;
+                    } else {
+                        double mul = op1 * op2;
+                        x = x + mul;
+                    }
+                }
+                result[col * aRows + row] = x;
+            }
+        }
+        return RDataFactory.createDoubleVector(result, na.neverSeenNA(), new int[]{aRows, bCols});
+
+    }
+
+    protected static boolean matdouble(RAbstractVector vector1) {
+        return vector1.getElementClass() == RDouble.class && vector1.isMatrix();
+    }
+
 }
