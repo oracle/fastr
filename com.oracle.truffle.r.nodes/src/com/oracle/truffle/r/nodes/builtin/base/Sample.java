ed7879c04054319cd4fd642610d3f62965c8f5bd
Michael Haupt <michael.haupt@oracle.com>
2014-06-18 08:50:41 +0200

    copyright fixes

--- com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/builtin/base/Sample.java
+++ com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/builtin/base/Sample.java
@@ -4,10 +4,10 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
  * Copyright (c) 1995, 1996, Robert Gentleman and Ross Ihaka
- * Copyright (c) 1997--2012, The R Core Team
- * Copyright (c) 2003--2008, The R Foundation
- * Copyright (c) 2012-2013, Purdue University
- * Copyright (c) 2013, 2014, Oracle and/or its affiliates
+ * Copyright (c) 1997-2012, The R Core Team
+ * Copyright (c) 2003-2008, The R Foundation
+ * Copyright (c) 2014, Purdue University
+ * Copyright (c) 2014, Oracle and/or its affiliates
  *
  * All rights reserved.
  */
