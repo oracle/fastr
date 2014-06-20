98b3723a9f0efb7787fe5a4a32e05b92456e8fe5
Adam Welc <adam.welc@oracle.com>
2014-06-20 13:13:13 -0700

    Fixed copyright attribution for previously added file.

--- com.oracle.truffle.r.native/src/fft.c
+++ com.oracle.truffle.r.native/src/fft.c
@@ -3,6 +3,7 @@
  * Version 2. You may review the terms of this license at
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
+ * Copyright (c) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
  * Copyright (c) 1995-2012, The R Core Team
  * Copyright (c) 2003, The R Foundation
  * Copyright (c) 2014, Oracle and/or its affiliates
