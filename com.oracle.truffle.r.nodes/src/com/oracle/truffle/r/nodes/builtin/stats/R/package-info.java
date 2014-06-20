ae7da12ec20287c97feff21056240dfff59c503e
Adam Welc <adam.welc@oracle.com>
2014-06-20 09:12:55 -0700

    Fixed copyright headers.

--- com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/builtin/stats/R/package-info.java
+++ com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/builtin/stats/R/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,3 +26,4 @@
  * mechanism on system startup.
  */
 package com.oracle.truffle.r.nodes.builtin.stats.R;
+
