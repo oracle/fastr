18d7768221a3a3441d57fd04ba7c1e63aa6a0c3a
Adam Welc <adam.welc@oracle.com>
2014-08-08 13:48:33 -0700

    Updated copyright info.

--- com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/builtin/base/Unlist.java
+++ com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/builtin/base/Unlist.java
@@ -1,24 +1,13 @@
 /*
- * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * This material is distributed under the GNU General Public License
+ * Version 2. You may review the terms of this license at
+ * http://www.gnu.org/licenses/gpl-2.0.html
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * Copyright (c) 1995-2012, The R Core Team
+ * Copyright (c) 2003, The R Foundation
+ * Copyright (c) 2013, 2014, Oracle and/or its affiliates
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * All rights reserved.
  */
 package com.oracle.truffle.r.nodes.builtin.base;
 
@@ -40,6 +29,8 @@ import com.oracle.truffle.r.nodes.builtin.base.UnlistFactory.RecursiveLengthFact
 @RBuiltin(name = "unlist", kind = SUBSTITUTE)
 // TODO INTERNAL
 public abstract class Unlist extends RBuiltinNode {
+
+    // portions of the algorithm were transcribed from GNU R
 
     private static final Object[] PARAMETER_NAMES = new Object[]{"x", "recursive", "use.names"};
 
