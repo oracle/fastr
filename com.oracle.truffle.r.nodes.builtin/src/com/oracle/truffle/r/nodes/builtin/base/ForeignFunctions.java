5ba401603a600ae60240361e88842e17ee13f481
Mick Jordan <mick.jordan@oracle.com>
2014-08-26 15:07:10 -0700

    error string correction for .Call

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/ForeignFunctions.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/ForeignFunctions.java
@@ -195,7 +195,7 @@ public class ForeignFunctions {
             Object[] argValues = ((RArgsValuesAndNames) args).getValues();
             SymbolInfo symbolInfo = DLL.findSymbolInfo(f, null);
             if (symbolInfo == null) {
-                throw RError.error(getEncapsulatingSourceSection(), RError.Message.SYMBOL_NOT_IN_TABLE, f);
+                throw RError.error(getEncapsulatingSourceSection(), RError.Message.C_SYMBOL_NOT_IN_TABLE, f);
             }
             boolean dupArgs = RRuntime.fromLogical(dup);
             boolean checkNA = RRuntime.fromLogical(naok);
@@ -436,7 +436,7 @@ public class ForeignFunctions {
         public Object callNamedFunctionWithPackage(String name, RArgsValuesAndNames args, String packageName) {
             SymbolInfo symbolInfo = DLL.findSymbolInfo(name, packageName);
             if (symbolInfo == null) {
-                throw RError.error(getEncapsulatingSourceSection(), Message.GENERIC, ".Call %s not found", name);
+                throw RError.error(getEncapsulatingSourceSection(), Message.C_SYMBOL_NOT_IN_TABLE, name);
             }
             try {
                 return RFFIFactory.getRFFI().getCallRFFI().invokeCall(symbolInfo, args.getValues());
