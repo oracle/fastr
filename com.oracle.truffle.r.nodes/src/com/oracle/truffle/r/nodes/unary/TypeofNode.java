db15666918fa4dfeb4b574484385dcdc5c7bbd2e
Gero Leinemann <gero.leinemann@oracle.com>
2014-09-04 12:43:29 +0200

    Cleanup in "typeof" after conversion to INTERNAL

--- com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/unary/TypeofNode.java
+++ com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/unary/TypeofNode.java
@@ -33,11 +33,6 @@ import com.oracle.truffle.r.runtime.env.*;
 
 @SuppressWarnings("unused")
 public abstract class TypeofNode extends UnaryNode {
-    public static final String ARG_NAME = "x";
-
-    private static final int MORE_THEN_2 = -1;
-
-    @Child private ToStringNode toStringNode = null;
 
     public abstract String execute(VirtualFrame frame, Object x);
 
@@ -146,46 +141,6 @@ public abstract class TypeofNode extends UnaryNode {
         return RRuntime.TYPE_PAIR_LIST;
     }
 
-    @Specialization
-    protected String typeofVarArgs0(RMissing args) {
-        // RArgsValuesAndNames/"..." with length 0 is RMissing
-        throw RError.error(getSourceSection(), RError.Message.ARGUMENT_MISSING, ARG_NAME);
-    }
-
-    @Specialization(guards = "isRArgsValuesAndNamesOfLength1")
-    protected String typeofVarArgs1(VirtualFrame frame, RArgsValuesAndNames args) {
-        // TODO Does Truffle allow recursive calling of nodes??
-        return this.execute(frame, args.getValues()[0]);
-    }
-
-    @Specialization(guards = "isRArgsValuesAndNamesOfLength2")
-    protected String typeofVarArgs2(VirtualFrame frame, RArgsValuesAndNames args) {
-        throw RError.error(getSourceSection(), RError.Message.UNUSED_ARGUMENT, args.getNames()[1]);
-    }
-
-    @Specialization(guards = "isRArgsValuesAndNamesOfLengthGT2")
-    @SlowPath
-    protected String typeofVarArgsGT2(VirtualFrame frame, RArgsValuesAndNames args) {
-        // More then 1 unused argument: Create argument string
-        setupToString();
-        StringBuilder b = new StringBuilder();
-        for (int i = 0; i < args.getNames().length; i++) {
-            Object value = args.getValues()[i];
-            if (value instanceof RPromise) {
-                RPromise promise = (RPromise) value;
-                RNode expr = (RNode) promise.getRep();
-                b.append(expr.getSourceSection().toString());
-            } else {
-                b.append(toStringNode.execute(frame, value));
-            }
-            if (i < args.getNames().length - 1) {
-                b.append(ToStringNode.DEFAULT_SEPARATOR);
-            }
-        }
-
-        throw RError.error(getSourceSection(), RError.Message.UNUSED_ARGUMENTS, b.toString());
-    }
-
     @Specialization(guards = "isFunctionBuiltin")
     protected String typeofBuiltin(RFunction obj) {
         return "builtin";
@@ -201,32 +156,7 @@ public abstract class TypeofNode extends UnaryNode {
         return "language";
     }
 
-    private void setupToString() {
-        if (toStringNode == null) {
-            CompilerDirectives.transferToInterpreterAndInvalidate();
-            toStringNode = insert(ToStringNodeFactory.create(null));
-            toStringNode.setSeparator(ToStringNode.DEFAULT_SEPARATOR);
-        }
-    }
-
     public static boolean isFunctionBuiltin(RFunction fun) {
         return fun.isBuiltin();
-    }
-
-    public static boolean isRArgsValuesAndNamesOfLength1(RArgsValuesAndNames args) {
-        return isRArgsValuesAndNamesOfLength(args, 1);
-    }
-
-    public static boolean isRArgsValuesAndNamesOfLength2(RArgsValuesAndNames args) {
-        return isRArgsValuesAndNamesOfLength(args, 2);
-    }
-
-    public static boolean isRArgsValuesAndNamesOfLengthGT2(RArgsValuesAndNames args) {
-        return isRArgsValuesAndNamesOfLength(args, MORE_THEN_2);
-    }
-
-    public static boolean isRArgsValuesAndNamesOfLength(RArgsValuesAndNames args, int len) {
-        int actLength = args.length();
-        return len == MORE_THEN_2 || actLength == len;
     }
 }
