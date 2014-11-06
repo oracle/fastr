cc92a853897929a267d41e8d230314c3e3d6afb7
Michael Haupt <michael.haupt@oracle.com>
2014-11-06 14:53:29 +0100

    copyright fixes

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/stats/StatsUtil.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/stats/StatsUtil.java
@@ -3,8 +3,9 @@
  * Version 2. You may review the terms of this license at
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Copyright (c) 1995-2012, The R Core Team
- * Copyright (c) 2003, The R Foundation
+ * Copyright (C) 1998 Ross Ihaka
+ * Copyright (c) 1998--2012, The R Core Team
+ * Copyright (c) 2004, The R Foundation
  * Copyright (c) 2013, 2014, Oracle and/or its affiliates
  *
  * All rights reserved.
