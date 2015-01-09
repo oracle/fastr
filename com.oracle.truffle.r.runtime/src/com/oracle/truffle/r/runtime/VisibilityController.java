96cdcc2da90a7538effdb8cf7de7c1a92927bc56
Mick Jordan <mick.jordan@oracle.com>
2015-01-09 11:28:50 -0800

    switch cleanup, copyright fixes

--- com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/VisibilityController.java
+++ com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/VisibilityController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -51,6 +51,16 @@ public interface VisibilityController {
     default void controlVisibility() {
         if (!RContext.isHeadless()) {
             RContext.setVisible(getVisibility());
+        }
+    }
+
+    /**
+     * Force the visibility to {@code state}. Useful for builtins where the visibility depends on
+     * the result, e.g. {@code switch}.
+     */
+    default void forceVisibility(boolean state) {
+        if (!RContext.isHeadless()) {
+            RContext.setVisible(state);
         }
     }
 }
