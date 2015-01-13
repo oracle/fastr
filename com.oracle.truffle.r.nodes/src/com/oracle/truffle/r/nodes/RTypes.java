468039b1167c5ef21628f7479ef448f90f66eaaf
Christian Humer <christian.humer@gmail.com>
2015-01-13 16:31:30 +0100

    R: fix import and update checkstyle configuration for DSL generated code.

--- com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/RTypes.java
+++ com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/RTypes.java
@@ -23,7 +23,6 @@
 package com.oracle.truffle.r.nodes;
 
 import com.oracle.truffle.api.dsl.*;
-import com.oracle.truffle.api.dsl.internal.*;
 import com.oracle.truffle.api.frame.*;
 import com.oracle.truffle.r.runtime.*;
 import com.oracle.truffle.r.runtime.data.*;
