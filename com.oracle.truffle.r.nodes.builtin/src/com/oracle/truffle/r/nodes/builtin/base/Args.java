cc13f40d6c3f101235da02c95879a5b0f21dcea0
Mick Jordan <mick.jordan@oracle.com>
2015-02-09 11:48:49 -0800

    use explicit import to avoid compilation error in Eclipse after clean

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Args.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Args.java
@@ -30,7 +30,7 @@ import com.oracle.truffle.r.nodes.*;
 import com.oracle.truffle.r.nodes.access.*;
 import com.oracle.truffle.r.nodes.builtin.*;
 import com.oracle.truffle.r.nodes.builtin.base.FrameFunctionsFactory.ParentFrameFactory;
-import com.oracle.truffle.r.nodes.builtin.base.GetFunctionsFactory.*;
+import com.oracle.truffle.r.nodes.builtin.base.GetFunctionsFactory.GetFactory;
 import com.oracle.truffle.r.nodes.function.*;
 import com.oracle.truffle.r.runtime.*;
 import com.oracle.truffle.r.runtime.data.*;
