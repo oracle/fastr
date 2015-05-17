963e5e1322a7cf41f3eb082820e47e2aa2b205f1
Christian Humer <christian.humer@oracle.com>
2015-05-17 19:49:53 +0200

    Fixed wrong completeness for file functions.

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/FileFunctions.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/FileFunctions.java
@@ -86,7 +86,7 @@ public class FileFunctions {
              * There are two simple (non-trivial) cases and one tricky 1. 1. Append one or more
              * files to a single file (len1 == 1, len2 >= 1) 2. Append one file to one file for
              * several files (len1 == len2)
-             *
+             * 
              * The tricky case is when len1 > 1 && len2 > len1. E.g. f1,f2 <- g1,g2,g3 In this case,
              * this is really f1,f2,f1 <- g1,g2,g3
              */
@@ -225,7 +225,7 @@ public class FileFunctions {
              * the information. The R closure that called the .Internal turns the result into a
              * dataframe and sets the row.names attributes to the paths in vec. It also updates the
              * mtime, ctime, atime fields using .POSIXct.
-             *
+             * 
              * We try to use the JDK classes, even though they provide a more abstract interface
              * than R. In particular there seems to be no way to get the uid/gid values. We might be
              * better off justing using a native call.
@@ -281,7 +281,7 @@ public class FileFunctions {
                 } else {
                     for (int n = 0; n < Column.VALUES.length; n++) {
                         setNA(Column.VALUES[n], data, i);
-                        complete[n] = true;
+                        complete[n] = false;
                     }
                 }
             }
