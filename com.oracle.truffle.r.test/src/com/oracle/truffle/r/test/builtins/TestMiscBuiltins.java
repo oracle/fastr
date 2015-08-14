3f427fa01014c81ad2289fda78e3c12a3b83c1e4
Adam Welc <adam.welc@oracle.com>
2015-08-13 20:42:47 -0700

    Added a test to make sure that the channel/context implementation does not rot.

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestMiscBuiltins.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestMiscBuiltins.java
@@ -344,4 +344,9 @@ public class TestMiscBuiltins extends TestBase {
     public void testTypeConvert() {
         assertEval("{ x<-as.character(list(a=\"0\", b=\"0\", c=\"0.3\")); type.convert(x, as.is=FALSE) }");
     }
+
+    @Test
+    public void testChannels() {
+        assertEval("{ if (length(grep(\"FastR\", R.Version()$version.string)) == 1) { ch <- fastr.channel.create(1L); cx <- fastr.context.create(\"SHARED_NOTHING\"); fastr.context.spawn(cx, \"ch <- fastr.channel.get(1L); x<-fastr.channel.receive(ch); x[1]<-7; fastr.channel.send(ch, x)\"); y<-c(42); fastr.channel.send(ch, y); x<-fastr.channel.receive(ch); fastr.context.join(cx); fastr.channel.close(ch); print(c(x,y)) } else { print(c(7L, 42L)) } }");
+    }
 }
