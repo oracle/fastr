afb2d1a00b8a31758c7d5a76b72e91cb52d84468
Mick Jordan <mick.jordan@oracle.com>
2015-08-26 15:53:19 -0700

    partial impl of identical for functions

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Identical.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Identical.java
@@ -27,6 +27,7 @@ import static com.oracle.truffle.r.runtime.RBuiltinKind.*;
 import com.oracle.truffle.api.dsl.*;
 import com.oracle.truffle.api.utilities.*;
 import com.oracle.truffle.r.nodes.builtin.*;
+import com.oracle.truffle.r.nodes.function.*;
 import com.oracle.truffle.r.runtime.*;
 import com.oracle.truffle.r.runtime.data.*;
 import com.oracle.truffle.r.runtime.data.model.*;
@@ -34,14 +35,19 @@ import com.oracle.truffle.r.runtime.env.*;
 
 /**
  * Internal part of {@code identical}. The default values for args after {@code x} and {@code y} are
- * all set to {@code TRUE/FALSE} by the R wrapper.
+ * all default to {@code TRUE/FALSE} in the R wrapper.
  *
- * TODO Implement the full set of types.
+ * TODO Implement the full set of types. This will require refactoring the code so that a generic
+ * "identical" function can be called recursively to handle lists and language objects (and
+ * closures) GnuR compares attributes also. The general case is therefore slow but the fast path
+ * needs to be fast! The five defaulted logical arguments are supposed to be cast to logical and
+ * checked for NA (regardless of whether they are used).
  */
 @RBuiltin(name = "identical", kind = INTERNAL, parameterNames = {"x", "y", "num.eq", "single.NA", "attrib.as.set", "ignore.bytecode", "ignore.environment"})
 public abstract class Identical extends RBuiltinNode {
 
     private final ConditionProfile vecLengthProfile = ConditionProfile.createBinaryProfile();
+    private final ConditionProfile naArgsProfile = ConditionProfile.createBinaryProfile();
 
     @Specialization(guards = "isRNull(x) || isRNull(y)")
     protected byte doInternalIdentical(Object x, Object y,
@@ -116,6 +122,36 @@ public abstract class Identical extends RBuiltinNode {
         throw RError.nyi(this, "language objects not supported in 'identical'");
     }
 
+    @Specialization
+    byte doInternalIdentical(RFunction x, RFunction y, byte numEq, byte singleNA, byte attribAsSet, byte ignoreBytecode, byte ignoreEnvironment) {
+        controlVisibility();
+        if (naArgsProfile.profile(checkExtraArgsForNA(numEq, singleNA, attribAsSet, ignoreBytecode, ignoreEnvironment))) {
+            if (x == y) {
+                return RRuntime.LOGICAL_TRUE;
+            }
+            if (x.isBuiltin() && !y.isBuiltin() || y.isBuiltin() && !x.isBuiltin()) {
+                return RRuntime.LOGICAL_FALSE;
+            }
+            if (x.isBuiltin()) {
+                return RRuntime.asLogical(x.getRBuiltin() == y.getRBuiltin());
+            } else {
+                // closures
+                FunctionDefinitionNode fx = (FunctionDefinitionNode) x.getRootNode();
+                FunctionDefinitionNode fy = (FunctionDefinitionNode) y.getRootNode();
+                if (fx.getFormalArguments().getSignature().equals(fy.getFormalArguments().getSignature())) {
+                    /*
+                     * TODO check defaults and body and enclosing environment (unless
+                     * ignoreEnvironment=T)
+                     */
+                } else {
+                    return RRuntime.LOGICAL_FALSE;
+                }
+                throw RError.nyi(this, "identical(function)");
+            }
+        }
+        return RRuntime.LOGICAL_FALSE;
+    }
+
     @Specialization(guards = "!vectorsLists(x, y)")
     protected byte doInternalIdenticalGeneric(RAbstractVector x, RAbstractVector y,
                     // @formatter:off
@@ -187,6 +223,18 @@ public abstract class Identical extends RBuiltinNode {
 
     protected boolean vectorsLists(RAbstractVector x, RAbstractVector y) {
         return x instanceof RList && y instanceof RList;
+    }
+
+    protected boolean checkExtraArgsForNA(byte numEq, byte singleNA, byte attribAsSet, byte ignoreBytecode, byte ignoreEnvironment) {
+        return checkExtraArg(numEq, "num.eq") && checkExtraArg(singleNA, "single.NA") && checkExtraArg(attribAsSet, "attrib.as.set") && checkExtraArg(ignoreBytecode, "ignore.bytecode") &&
+                        checkExtraArg(ignoreEnvironment, "ignore.environment");
+    }
+
+    protected boolean checkExtraArg(byte value, String name) {
+        if (value == RRuntime.LOGICAL_NA) {
+            throw RError.error(this, RError.Message.INVALID_VALUE, name);
+        }
+        return true;
     }
 
 }
