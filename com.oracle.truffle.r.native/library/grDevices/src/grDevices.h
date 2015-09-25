4df9c477f8b7bfa74d584ca9d1322cc873744db3
Lukas Stadler <lukas.stadler@oracle.com>
2015-09-25 16:03:33 +0200

    copy code into graphics and grDevices packages

--- com.oracle.truffle.r.native/library/grDevices/src/grDevices.h
+++ com.oracle.truffle.r.native/library/grDevices/src/grDevices.h
@@ -29,92 +29,85 @@
 #define _(String) (String)
 #endif
 
-SEXP R_CreateAtVector(SEXP axp, SEXP usr, SEXP nint, SEXP is_log) { return NULL; }
-SEXP R_GAxisPars(SEXP usr, SEXP is_log, SEXP nintLog) { return NULL; }
+SEXP R_CreateAtVector(SEXP axp, SEXP usr, SEXP nint, SEXP is_log);
+SEXP R_GAxisPars(SEXP usr, SEXP is_log, SEXP nintLog);
 
-SEXP PicTeX(SEXP args) { return NULL; }
+SEXP PicTeX(SEXP);
 
-SEXP PostScript(SEXP args) { return NULL; }
-SEXP XFig(SEXP args) { return NULL; }
-SEXP PDF(SEXP args) { return NULL; }
-SEXP Type1FontInUse(SEXP arg1, SEXP arg2) { return NULL; }
-SEXP CIDFontInUse(SEXP arg1, SEXP arg2) { return NULL; }
+SEXP PostScript(SEXP);
+SEXP XFig(SEXP);
+SEXP PDF(SEXP);
+SEXP Type1FontInUse(SEXP, SEXP);
+SEXP CIDFontInUse(SEXP, SEXP);
 
 #ifndef _WIN32
-SEXP Quartz(SEXP args) { return NULL; }
-SEXP makeQuartzDefault() { return NULL; }
+SEXP Quartz(SEXP);
+SEXP makeQuartzDefault();
 
-SEXP X11(SEXP call, SEXP op, SEXP args, SEXP rho) { return NULL; }
-SEXP savePlot(SEXP call, SEXP op, SEXP args, SEXP rho) { return NULL; }
+SEXP X11(SEXP call, SEXP op, SEXP args, SEXP rho);
+SEXP savePlot(SEXP call, SEXP op, SEXP args, SEXP rho);
 #endif
 
-SEXP devCairo(SEXP args) { return NULL; }
+SEXP devCairo(SEXP);
 
 Rboolean
-PSDeviceDriver(pDevDesc dd, const char *file, const char *paper,
-	       const char *family, const char **afmpaths, const char *encoding,
-	       const char *bg, const char *fg, double width, double height,
-	       Rboolean horizontal, double ps,
-	       Rboolean onefile, Rboolean pagecentre, Rboolean printit,
-	       const char *cmd, const char *title, SEXP fonts,
-	       const char *colormodel, int useKern, Rboolean fillOddEven) { return FALSE; }
+PSDeviceDriver(pDevDesc, const char*, const char*, const char*,
+	       const char **, const char*, const char*, const char*,
+	       double, double, Rboolean, double, Rboolean, Rboolean,
+	       Rboolean, const char*, const char*, SEXP, const char*, int,
+	       Rboolean);
 
 Rboolean
-PDFDeviceDriver(pDevDesc dd, const char *file, const char *paper,
-		const char *family, const char **afmpaths,
-		const char *encoding,
-		const char *bg, const char *fg, double width, double height,
-		double ps, int onefile, int pagecentre,
-		const char *title, SEXP fonts,
-		int versionMajor, int versionMinor,
-		const char *colormodel, int dingbats, int useKern,
-		Rboolean fillOddEven, Rboolean useCompression) { return FALSE; }
+PDFDeviceDriver(pDevDesc, const char *, const char *, const char *,
+		const char **, const char *, const char *, const char *,
+		double, double, double, int, int, const char*, SEXP,
+		int, int, const char *, int, int, Rboolean, Rboolean);
 
 #ifdef _WIN32
-SEXP devga(SEXP) { return NULL; }
-SEXP savePlot(SEXP) { return NULL; }
-SEXP bringToTop(SEXP, SEXP) { return NULL; }
-SEXP msgWindow(SEXP, SEXP) { return NULL; }
+SEXP devga(SEXP);
+SEXP savePlot(SEXP);
+SEXP bringToTop(SEXP, SEXP);
+SEXP msgWindow(SEXP, SEXP);
 #endif
 
-SEXP devcap(SEXP args) { return NULL; }
-SEXP devcapture(SEXP args) { return NULL; }
-SEXP devcontrol(SEXP args) { return NULL; }
-SEXP devcopy(SEXP args) { return NULL; }
-SEXP devcur(SEXP args) { return NULL; }
-SEXP devdisplaylist(SEXP args) { return NULL; }
-SEXP devholdflush(SEXP args) { return NULL; }
-SEXP devnext(SEXP args) { return NULL; }
-SEXP devoff(SEXP args) { return NULL; }
-SEXP devprev(SEXP args) { return NULL; }
-SEXP devset(SEXP args) { return NULL; }
-SEXP devsize(SEXP args) { return NULL; }
+SEXP devcap(SEXP args);
+SEXP devcapture(SEXP args);
+SEXP devcontrol(SEXP args);
+SEXP devcopy(SEXP args);
+SEXP devcur(SEXP args);
+SEXP devdisplaylist(SEXP args);
+SEXP devholdflush(SEXP args);
+SEXP devnext(SEXP args);
+SEXP devoff(SEXP args);
+SEXP devprev(SEXP args);
+SEXP devset(SEXP args);
+SEXP devsize(SEXP args);
 
-SEXP chull(SEXP x) { return NULL; }
+SEXP chull(SEXP x);
 
-SEXP contourLines(SEXP call, SEXP op, SEXP args, SEXP rho) { return NULL; }
-SEXP getSnapshot(SEXP call, SEXP op, SEXP args, SEXP rho) { return NULL; }
-SEXP playSnapshot(SEXP call, SEXP op, SEXP args, SEXP rho) { return NULL; }
-SEXP getGraphicsEvent(SEXP call, SEXP op, SEXP args, SEXP rho) { return NULL; }
-SEXP getGraphicsEventEnv(SEXP call, SEXP op, SEXP args, SEXP rho) { return NULL; }
-SEXP setGraphicsEventEnv(SEXP call, SEXP op, SEXP args, SEXP rho) { return NULL; }
-SEXP devAskNewPage(SEXP call, SEXP op, SEXP args, SEXP env) { return NULL; }
+SEXP contourLines(SEXP call, SEXP op, SEXP args, SEXP rho);
+SEXP getSnapshot(SEXP call, SEXP op, SEXP args, SEXP rho);
+SEXP playSnapshot(SEXP call, SEXP op, SEXP args, SEXP rho);
+SEXP getGraphicsEvent(SEXP call, SEXP op, SEXP args, SEXP rho);
+SEXP getGraphicsEventEnv(SEXP call, SEXP op, SEXP args, SEXP rho);
+SEXP setGraphicsEventEnv(SEXP call, SEXP op, SEXP args, SEXP rho);
+SEXP devAskNewPage(SEXP call, SEXP op, SEXP args, SEXP env);
 
 #ifndef DEVWINDOWS
-SEXP rgb(SEXP r, SEXP g, SEXP b, SEXP a, SEXP MCV, SEXP nam) { return NULL; }
-SEXP hsv(SEXP h, SEXP s, SEXP v, SEXP a) { return NULL; }
-SEXP hcl(SEXP h, SEXP c, SEXP l, SEXP a, SEXP sfixup) { return NULL; }
-SEXP gray(SEXP lev, SEXP a) { return NULL; }
-SEXP colors(void) { return NULL; }
-SEXP col2rgb(SEXP colors, SEXP alpha) { return NULL; }
-SEXP palette(SEXP value) { return NULL; }
-SEXP palette2(SEXP value) { return NULL; }
-SEXP RGB2hsv(SEXP rgb) { return NULL; }
+SEXP rgb(SEXP r, SEXP g, SEXP b, SEXP a, SEXP MCV, SEXP nam);
+SEXP hsv(SEXP h, SEXP s, SEXP v, SEXP a);
+SEXP hcl(SEXP h, SEXP c, SEXP l, SEXP a, SEXP sfixup);
+SEXP gray(SEXP lev, SEXP a);
+SEXP colors(void);
+SEXP col2rgb(SEXP colors, SEXP alpha);
+SEXP palette(SEXP value);
+SEXP palette2(SEXP value);
+SEXP RGB2hsv(SEXP rgb);
 #endif
 
-unsigned int inRGBpar3(SEXP arg1, int arg2, unsigned int arg3) { return 0; }
-const char *incol2name(unsigned int col) { return NULL; }
-unsigned int inR_GE_str2col(const char *s) { return 0; }
-void initPalette(void) { }
+unsigned int inRGBpar3(SEXP, int, unsigned int);
+const char *incol2name(unsigned int col);
+unsigned int inR_GE_str2col(const char *s);
+void initPalette(void);
 
 
---
+++ com.oracle.truffle.r.native/library/graphics/src/Defn.h
@@ -0,0 +1,3 @@
+// replacing the original Defn.h file
+
+#include "../../grDevices/src/Defn.h"
