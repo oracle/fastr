7b83d2800a55e4c9f7c1cbb07da10db09ae3f2fd
Lukas Stadler <lukas.stadler@oracle.com>
2015-09-30 16:42:40 +0200

    include selected functions from coerce.c and util.c from GNU-R

--- com.oracle.truffle.r.native/fficall/jni/src/typecoerce.c
+++ com.oracle.truffle.r.native/fficall/jni/src/typecoerce.c
@@ -20,17 +20,20 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 #include "rffiutils.h"
 
+#define _(Source) (Source)
+
 static jmethodID Rf_asIntegerMethodID;
-static jmethodID Rf_asRealMethodID;
+//static jmethodID Rf_asRealMethodID;
 static jmethodID Rf_asCharMethodID;
 static jmethodID Rf_asLogicalMethodID;
 static jmethodID Rf_PairToVectorListMethodID;
 
 void init_typecoerce(JNIEnv *env) {
 	Rf_asIntegerMethodID = checkGetMethodID(env, CallRFFIHelperClass, "Rf_asInteger", "(Ljava/lang/Object;)I", 1);
-	Rf_asRealMethodID = checkGetMethodID(env, CallRFFIHelperClass, "Rf_asReal", "(Ljava/lang/Object;)D", 1);
+//	Rf_asRealMethodID = checkGetMethodID(env, CallRFFIHelperClass, "Rf_asReal", "(Ljava/lang/Object;)D", 1);
 	Rf_asCharMethodID = checkGetMethodID(env, CallRFFIHelperClass, "Rf_asChar", "(Ljava/lang/Object;)Ljava/lang/String;", 1);
 	Rf_asLogicalMethodID = checkGetMethodID(env, CallRFFIHelperClass, "Rf_asLogical", "(Ljava/lang/Object;)I", 1);
 	Rf_PairToVectorListMethodID = checkGetMethodID(env, CallRFFIHelperClass, "Rf_PairToVectorList", "(Ljava/lang/Object;)Ljava/lang/Object;", 1);
@@ -41,11 +44,6 @@ SEXP Rf_asChar(SEXP x){
 	JNIEnv *thisenv = getEnv();
 	SEXP result = (*thisenv)->CallStaticObjectMethod(thisenv, CallRFFIHelperClass, Rf_asCharMethodID, x);
 	return checkRef(thisenv, result);
-}
-
-SEXP Rf_coerceVector(SEXP x, SEXPTYPE t){
-	unimplemented("Rf_coerceVector");
-	return NULL;
 }
 
 SEXP Rf_PairToVectorList(SEXP x){
@@ -76,13 +74,411 @@ int Rf_asInteger(SEXP x) {
 	return (*thisenv)->CallStaticIntMethod(thisenv, CallRFFIHelperClass, Rf_asIntegerMethodID, x);
 }
 
-double Rf_asReal(SEXP x) {
-	TRACE(TARG1, x);
-	JNIEnv *thisenv = getEnv();
-	return (*thisenv)->CallStaticDoubleMethod(thisenv, CallRFFIHelperClass, Rf_asRealMethodID, x);
-}
+//double Rf_asReal(SEXP x) {
+//	TRACE(TARG1, x);
+//	JNIEnv *thisenv = getEnv();
+//	return (*thisenv)->CallStaticDoubleMethod(thisenv, CallRFFIHelperClass, Rf_asRealMethodID, x);
+//}
 
 Rcomplex Rf_asComplex(SEXP x){
 	unimplemented("Rf_asLogical");
 	Rcomplex c; return c;
+}
+
+
+// selected functions from coerce.c:
+
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 1995,1996  Robert Gentleman, Ross Ihaka
+ *  Copyright (C) 1997-2013  The R Core Team
+ *  Copyright (C) 2003-2009 The R Foundation
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
+
+
+/* Coercion warnings will be OR'ed : */
+#define WARN_NA	   1
+#define WARN_INACC 2
+#define WARN_IMAG  4
+#define WARN_RAW  8
+
+void CoercionWarning(int warn)
+{
+/* FIXME: Use
+   =====
+   WarningMessage(R_NilValue, WARNING_....);
+*/
+    if (warn & WARN_NA)
+	warning(_("NAs introduced by coercion"));
+    if (warn & WARN_INACC)
+	warning(_("inaccurate integer conversion in coercion"));
+    if (warn & WARN_IMAG)
+	warning(_("imaginary parts discarded in coercion"));
+    if (warn & WARN_RAW)
+	warning(_("out-of-range values treated as 0 in coercion to raw"));
+}
+
+double
+RealFromLogical(int x, int *warn)
+{
+    return (x == NA_LOGICAL) ?
+	NA_REAL : x;
+}
+
+double
+RealFromInteger(int x, int *warn)
+{
+    if (x == NA_INTEGER)
+	return NA_REAL;
+    else
+	return x;
+}
+
+static SEXP coerceToReal(SEXP v)
+{
+    SEXP ans;
+    int warn = 0;
+    R_xlen_t i, n;
+    PROTECT(ans = allocVector(REALSXP, n = XLENGTH(v)));
+#ifdef R_MEMORY_PROFILING
+    if (RTRACE(v)){
+       memtrace_report(v,ans);
+       SET_RTRACE(ans,1);
+    }
+#endif
+    DUPLICATE_ATTRIB(ans, v);
+    switch (TYPEOF(v)) {
+    case LGLSXP:
+	for (i = 0; i < n; i++) {
+//	    if ((i+1) % NINTERRUPT == 0) R_CheckUserInterrupt();
+	    REAL(ans)[i] = RealFromLogical(LOGICAL(v)[i], &warn);
+	}
+	break;
+    case INTSXP:
+	for (i = 0; i < n; i++) {
+//	    if ((i+1) % NINTERRUPT == 0) R_CheckUserInterrupt();
+	    REAL(ans)[i] = RealFromInteger(INTEGER(v)[i], &warn);
+	}
+	break;
+    case CPLXSXP:
+	for (i = 0; i < n; i++) {
+//	    if ((i+1) % NINTERRUPT == 0) R_CheckUserInterrupt();
+	    REAL(ans)[i] = RealFromComplex(COMPLEX(v)[i], &warn);
+	}
+	break;
+    case STRSXP:
+	for (i = 0; i < n; i++) {
+//	    if ((i+1) % NINTERRUPT == 0) R_CheckUserInterrupt();
+	    REAL(ans)[i] = RealFromString(STRING_ELT(v, i), &warn);
+	}
+	break;
+    case RAWSXP:
+	for (i = 0; i < n; i++) {
+//	    if ((i+1) % NINTERRUPT == 0) R_CheckUserInterrupt();
+	    REAL(ans)[i] = RealFromInteger((int)RAW(v)[i], &warn);
+	}
+	break;
+    default:
+	UNIMPLEMENTED_TYPE("coerceToReal", v);
+    }
+    if (warn) CoercionWarning(warn);
+    UNPROTECT(1);
+    return ans;
+}
+
+static SEXP coerceToPairList(SEXP v)
+{
+    SEXP ans, ansp;
+    int i, n;
+    n = LENGTH(v); /* limited to len */
+    PROTECT(ansp = ans = allocList(n));
+    for (i = 0; i < n; i++) {
+	switch (TYPEOF(v)) {
+	case LGLSXP:
+	    SETCAR(ansp, allocVector(LGLSXP, 1));
+	    INTEGER(CAR(ansp))[0] = INTEGER(v)[i];
+	    break;
+	case INTSXP:
+	    SETCAR(ansp, allocVector(INTSXP, 1));
+	    INTEGER(CAR(ansp))[0] = INTEGER(v)[i];
+	    break;
+	case REALSXP:
+	    SETCAR(ansp, allocVector(REALSXP, 1));
+	    REAL(CAR(ansp))[0] = REAL(v)[i];
+	    break;
+	case CPLXSXP:
+	    SETCAR(ansp, allocVector(CPLXSXP, 1));
+	    COMPLEX(CAR(ansp))[0] = COMPLEX(v)[i];
+	    break;
+	case STRSXP:
+	    SETCAR(ansp, ScalarString(STRING_ELT(v, i)));
+	    break;
+	case RAWSXP:
+	    SETCAR(ansp, allocVector(RAWSXP, 1));
+	    RAW(CAR(ansp))[0] = RAW(v)[i];
+	    break;
+	case VECSXP:
+	    SETCAR(ansp, VECTOR_ELT(v, i));
+	    break;
+	case EXPRSXP:
+	    SETCAR(ansp, VECTOR_ELT(v, i));
+	    break;
+	default:
+	    UNIMPLEMENTED_TYPE("coerceToPairList", v);
+	}
+	ansp = CDR(ansp);
+    }
+    ansp = getAttrib(v, R_NamesSymbol);
+    if (ansp != R_NilValue)
+	setAttrib(ans, R_NamesSymbol, ansp);
+    UNPROTECT(1);
+    return (ans);
+}
+
+/* Coerce a pairlist to the given type */
+static SEXP coercePairList(SEXP v, SEXPTYPE type)
+{
+    int i, n=0;
+    SEXP rval= R_NilValue, vp, names;
+
+    /* Hmm, this is also called to LANGSXP, and coerceVector already
+       did the check of TYPEOF(v) == type */
+    if(type == LISTSXP) return v;/* IS pairlist */
+
+    names = v;
+    if (type == EXPRSXP) {
+	PROTECT(rval = allocVector(type, 1));
+	SET_VECTOR_ELT(rval, 0, v);
+	UNPROTECT(1);
+	return rval;
+    }
+    else if (type == STRSXP) {
+	n = length(v);
+	PROTECT(rval = allocVector(type, n));
+	for (vp = v, i = 0; vp != R_NilValue; vp = CDR(vp), i++) {
+	    if (isString(CAR(vp)) && length(CAR(vp)) == 1)
+		SET_STRING_ELT(rval, i, STRING_ELT(CAR(vp), 0));
+	    else
+		SET_STRING_ELT(rval, i, STRING_ELT(deparse1line(CAR(vp), 0), 0));
+	}
+    }
+    else if (type == VECSXP) {
+	rval = PairToVectorList(v);
+	return rval;
+    }
+    else if (isVectorizable(v)) {
+	n = length(v);
+	PROTECT(rval = allocVector(type, n));
+	switch (type) {
+	case LGLSXP:
+	    for (i = 0, vp = v; i < n; i++, vp = CDR(vp))
+		LOGICAL(rval)[i] = asLogical(CAR(vp));
+	    break;
+	case INTSXP:
+	    for (i = 0, vp = v; i < n; i++, vp = CDR(vp))
+		INTEGER(rval)[i] = asInteger(CAR(vp));
+	    break;
+	case REALSXP:
+	    for (i = 0, vp = v; i < n; i++, vp = CDR(vp))
+		REAL(rval)[i] = asReal(CAR(vp));
+	    break;
+	case CPLXSXP:
+	    for (i = 0, vp = v; i < n; i++, vp = CDR(vp))
+		COMPLEX(rval)[i] = asComplex(CAR(vp));
+	    break;
+	case RAWSXP:
+	    for (i = 0, vp = v; i < n; i++, vp = CDR(vp))
+		RAW(rval)[i] = (Rbyte) asInteger(CAR(vp));
+	    break;
+	default:
+	    UNIMPLEMENTED_TYPE("coercePairList", v);
+	}
+    }
+    else
+	error(_("'pairlist' object cannot be coerced to type '%s'"),
+	      type2char(type));
+
+    /* If any tags are non-null then we */
+    /* need to add a names attribute. */
+    for (vp = v, i = 0; vp != R_NilValue; vp = CDR(vp))
+	if (TAG(vp) != R_NilValue)
+	    i = 1;
+
+    if (i) {
+	i = 0;
+	names = allocVector(STRSXP, n);
+	for (vp = v; vp != R_NilValue; vp = CDR(vp), i++)
+	    if (TAG(vp) != R_NilValue)
+		SET_STRING_ELT(names, i, PRINTNAME(TAG(vp)));
+	setAttrib(rval, R_NamesSymbol, names);
+    }
+    UNPROTECT(1);
+    return rval;
+}
+
+SEXP Rf_coerceVector(SEXP v, SEXPTYPE type)
+{
+    SEXP op, vp, ans = R_NilValue;	/* -Wall */
+    int i,n;
+
+    if (TYPEOF(v) == type)
+	return v;
+    /* code to allow classes to extend ENVSXP, SYMSXP, etc */
+    if(IS_S4_OBJECT(v) && TYPEOF(v) == S4SXP) {
+	SEXP vv = R_getS4DataSlot(v, ANYSXP);
+	if(vv == R_NilValue)
+	  error(_("no method for coercing this S4 class to a vector"));
+	else if(TYPEOF(vv) == type)
+	  return vv;
+	v = vv;
+    }
+
+    switch (TYPEOF(v)) {
+#ifdef NOTYET
+    case NILSXP:
+	ans = coerceNull(v, type);
+	break;
+#endif
+    case SYMSXP:
+	ans = coerceSymbol(v, type);
+	break;
+    case NILSXP:
+    case LISTSXP:
+	ans = coercePairList(v, type);
+	break;
+    case LANGSXP:
+	if (type != STRSXP) {
+	    ans = coercePairList(v, type);
+	    break;
+	}
+
+	/* This is mostly copied from coercePairList, but we need to
+	 * special-case the first element so as not to get operators
+	 * put in backticks. */
+	n = length(v);
+	PROTECT(ans = allocVector(type, n));
+	if (n == 0) break; /* Can this actually happen? */
+	i = 0;
+	op = CAR(v);
+	/* The case of practical relevance is "lhs ~ rhs", which
+	 * people tend to split using as.character(), modify, and
+	 * paste() back together. However, we might as well
+	 * special-case all symbolic operators here. */
+	if (TYPEOF(op) == SYMSXP) {
+	    SET_STRING_ELT(ans, i, PRINTNAME(op));
+	    i++;
+	    v = CDR(v);
+	}
+
+	/* The distinction between strings and other elements was
+	 * here "always", but is really dubious since it makes x <- a
+	 * and x <- "a" come out identical. Won't fix just now. */
+	for (vp = v;  vp != R_NilValue; vp = CDR(vp), i++) {
+	    if (isString(CAR(vp)) && length(CAR(vp)) == 1)
+		SET_STRING_ELT(ans, i, STRING_ELT(CAR(vp), 0));
+	    else
+		SET_STRING_ELT(ans, i, STRING_ELT(deparse1line(CAR(vp), 0), 0));
+	}
+	UNPROTECT(1);
+	break;
+    case VECSXP:
+    case EXPRSXP:
+	ans = coerceVectorList(v, type);
+	break;
+    case ENVSXP:
+	error(_("environments cannot be coerced to other types"));
+	break;
+    case LGLSXP:
+    case INTSXP:
+    case REALSXP:
+    case CPLXSXP:
+    case STRSXP:
+    case RAWSXP:
+
+#define COERCE_ERROR_STRING "cannot coerce type '%s' to vector of type '%s'"
+
+#define COERCE_ERROR							\
+	error(_(COERCE_ERROR_STRING), type2char(TYPEOF(v)), type2char(type))
+
+	switch (type) {
+	case SYMSXP:
+	    ans = coerceToSymbol(v);	    break;
+	case LGLSXP:
+	    ans = coerceToLogical(v);	    break;
+	case INTSXP:
+	    ans = coerceToInteger(v);	    break;
+	case REALSXP:
+	    ans = coerceToReal(v);	    break;
+	case CPLXSXP:
+	    ans = coerceToComplex(v);	    break;
+	case RAWSXP:
+	    ans = coerceToRaw(v);	    break;
+	case STRSXP:
+	    ans = coerceToString(v);	    break;
+	case EXPRSXP:
+	    ans = coerceToExpression(v);    break;
+	case VECSXP:
+	    ans = coerceToVectorList(v);    break;
+	case LISTSXP:
+	    ans = coerceToPairList(v);	    break;
+	default:
+	    COERCE_ERROR;
+	}
+	break;
+    default:
+	COERCE_ERROR;
+    }
+    return ans;
+}
+#undef COERCE_ERROR
+
+
+double Rf_asReal(SEXP x)
+{
+    int warn = 0;
+    double res;
+
+    if (isVectorAtomic(x) && XLENGTH(x) >= 1) {
+	switch (TYPEOF(x)) {
+	case LGLSXP:
+	    res = RealFromLogical(LOGICAL(x)[0], &warn);
+	    CoercionWarning(warn);
+	    return res;
+	case INTSXP:
+	    res = RealFromInteger(INTEGER(x)[0], &warn);
+	    CoercionWarning(warn);
+	    return res;
+	case REALSXP:
+	    return REAL(x)[0];
+	case CPLXSXP:
+	    res = RealFromComplex(COMPLEX(x)[0], &warn);
+	    CoercionWarning(warn);
+	    return res;
+	case STRSXP:
+	    res = RealFromString(STRING_ELT(x, 0), &warn);
+	    CoercionWarning(warn);
+	    return res;
+	default:
+	    UNIMPLEMENTED_TYPE("asReal", x);
+	}
+    } else if(TYPEOF(x) == CHARSXP) {
+	res = RealFromString(x, &warn);
+	CoercionWarning(warn);
+	return res;
+    }
+    return NA_REAL;
 }
--- com.oracle.truffle.r.native/fficall/jni/src/unimplemented.c
+++ com.oracle.truffle.r.native/fficall/jni/src/unimplemented.c
@@ -21,3 +21,119 @@
  * questions.
  */
 #include "rffiutils.h"
+
+#include "Defn.h"
+#include <Print.h>
+
+Rboolean known_to_be_latin1 = FALSE;
+Rboolean known_to_be_utf8 = FALSE;
+
+
+
+int R_cairoCdynload(int local, int now)
+{
+	unimplemented("R_cairoCdynload");
+    return 0;
+}
+
+SEXP do_X11(SEXP call, SEXP op, SEXP args, SEXP rho)
+{
+	unimplemented("do_X11");
+    return R_NilValue;
+}
+
+SEXP do_saveplot(SEXP call, SEXP op, SEXP args, SEXP rho)
+{
+	unimplemented("do_saveplot");
+    return R_NilValue;
+}
+
+
+SEXP do_getGraphicsEvent(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    unimplemented("do_getGraphicsEvent");
+    return R_NilValue;
+}
+
+
+SEXP do_setGraphicsEventEnv(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    unimplemented("do_setGraphicsEventEnv");
+    return R_NilValue;
+}
+
+SEXP do_getGraphicsEventEnv(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    unimplemented("do_getGraphicsEventEnv");
+    return R_NilValue;
+}
+
+const char *locale2charset(const char *locale)
+{
+	unimplemented("locale2charset");
+	return NULL;
+}
+
+void setup_RdotApp(void) {
+	unimplemented("setup_RdotApp");
+}
+
+const char *Rf_EncodeComplex(Rcomplex x, int wr, int dr, int er, int wi, int di, int ei, char cdec)
+{
+	unimplemented("Rf_EncodeComplex");
+	return NULL;
+}
+
+const char *Rf_EncodeInteger(int x, int w)
+{
+	unimplemented("Rf_EncodeInteger");
+	return NULL;
+}
+
+const char *Rf_EncodeLogical(int x, int w)
+{
+	unimplemented("Rf_EncodeLogical");
+	return NULL;
+}
+
+// from printutils.c
+#ifndef min
+#define min(a, b) (((a)<(b))?(a):(b))
+#endif
+#define NB 1000
+const char *Rf_EncodeReal(double x, int w, int d, int e, char cdec)
+{
+    static char buff[NB];
+    char *p, fmt[20];
+
+    /* IEEE allows signed zeros (yuck!) */
+    if (x == 0.0) x = 0.0;
+    if (!R_FINITE(x)) {
+	if(ISNA(x)) snprintf(buff, NB, "%*s", min(w, (NB-1)), CHAR(R_print.na_string));
+	else if(ISNAN(x)) snprintf(buff, NB, "%*s", min(w, (NB-1)), "NaN");
+	else if(x > 0) snprintf(buff, NB, "%*s", min(w, (NB-1)), "Inf");
+	else snprintf(buff, NB, "%*s", min(w, (NB-1)), "-Inf");
+    }
+    else if (e) {
+	if(d) {
+	    sprintf(fmt,"%%#%d.%de", min(w, (NB-1)), d);
+	    snprintf(buff, NB, fmt, x);
+	}
+	else {
+	    sprintf(fmt,"%%%d.%de", min(w, (NB-1)), d);
+	    snprintf(buff, NB, fmt, x);
+	}
+    }
+    else { /* e = 0 */
+	sprintf(fmt,"%%%d.%df", min(w, (NB-1)), d);
+	snprintf(buff, NB, fmt, x);
+    }
+    buff[NB-1] = '\0';
+
+    if(cdec != '.')
+      for(p = buff; *p; p++) if(*p == '.') *p = cdec;
+
+    return buff;
+}
+
+
