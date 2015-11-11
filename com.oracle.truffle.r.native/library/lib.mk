0aa343a27b74f3594cb3a2b6e1878843acb244c8
Christian Humer <christian.humer@oracle.com>
2015-11-11 14:01:53 +0100

    Fix unresolved symbols during startup.

--- com.oracle.truffle.r.native/library/lib.mk
+++ com.oracle.truffle.r.native/library/lib.mk
@@ -89,7 +89,7 @@ $(OBJ):
 
 $(LIB_PKG): $(C_OBJECTS) $(F_OBJECTS) $(PKGDIR)
 	mkdir -p $(LIBDIR)
-	$(DYLIB_LD) $(DYLIB_LDFLAGS) -o $(LIB_PKG) $(C_OBJECTS) $(F_OBJECTS)
+	$(DYLIB_LD) $(DYLIB_LDFLAGS) -o $(LIB_PKG) $(C_OBJECTS) $(F_OBJECTS) $(FLIBS_IN_SO)
 	mkdir -p $(FASTR_LIBDIR)/$(PKG)/libs
 	cp $(LIB_PKG) $(FASTR_LIBDIR)/$(PKG)/libs
 
