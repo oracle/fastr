3b9133afcc46c48a3c58b02c12577a6cfa1944d3
Mick Jordan <mick.jordan@oracle.com>
2015-11-19 13:35:02 -0800

    update r_benchmarks import

--- mx.fastr/mx_fastr.py
+++ mx.fastr/mx_fastr.py
@@ -522,7 +522,7 @@ def load_optional_suite(name, rev):
     return opt_suite
 
 _r_apptests_rev = '3193941e22ac8fd798ad5ae598865a6a8ba2a147'
-_r_benchmarks_rev = 'a432c123d456cd362ee37769f2aca5cc0544ede7'
+_r_benchmarks_rev = '4f051278d073092577b74c50a993b91d3b30783b'
 
 def mx_post_parse_cmd_line(opts):
     # load optional suites, r_apptests first so r_benchmarks can find it
