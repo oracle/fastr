feaca922b0a2560c0795a267ceadedb74be9a1de
Mick Jordan <mick.jordan@oracle.com>
2015-12-21 15:12:33 -0800

    switch from fastr.logchild system property to env var FASTR_LOGCHILD for logging system function calls

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/SystemFunction.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/SystemFunction.java
@@ -45,7 +45,7 @@ public abstract class SystemFunction extends RBuiltinNode {
         if (shell == null) {
             shell = "/bin/sh";
         }
-        if (System.getProperty("fastr.logchild") != null) {
+        if (RContext.getInstance().stateREnvVars.getMap().get("FASTR_LOGCHILD") != null) {
             System.out.printf("FastR system: %s -c %s%n", shell, command.getDataAt(0));
         }
         ProcessBuilder pb = new ProcessBuilder(shell, "-c", command.getDataAt(0));
