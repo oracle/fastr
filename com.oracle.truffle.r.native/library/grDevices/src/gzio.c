9f7f8329f967c4198ea63c04a2abf31447e496bf
Stefan Anzinger <stefan.anzinger@oracle.com>
2016-01-13 18:25:34 +0100

    Fix issues with compiling graphics/grDevices and fficall with non-gcc compilers

--- com.oracle.truffle.r.native/library/grDevices/src/gzio.c
+++ com.oracle.truffle.r.native/library/grDevices/src/gzio.c
@@ -6,14 +6,16 @@
  * Copyright (c) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
  * Copyright (c) 1995-2014, The R Core Team
  * Copyright (c) 2002-2008, The R Foundation
- * Copyright (c) 2015, Oracle and/or its affiliates
+ * Copyright (c) 2015, 2016, Oracle and/or its affiliates
  *
  * All rights reserved.
  */
 
 // This simply includes gzio.h (which actually contains code)
 
+
 #define f_tell ftell
+#define f_seek fseek
 #define warning Rf_warning
 
 #include GNUR_GZIO_H
