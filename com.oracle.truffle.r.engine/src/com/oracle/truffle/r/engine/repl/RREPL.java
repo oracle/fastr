b890f652350cd8b6d18f337e4e9784c645ac436d
Mick Jordan <mick.jordan@oracle.com>
2016-02-27 20:36:25 -0800

    REPLServer API fix

--- com.oracle.truffle.r.engine/src/com/oracle/truffle/r/engine/repl/RREPL.java
+++ com.oracle.truffle.r.engine/src/com/oracle/truffle/r/engine/repl/RREPL.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,14 +22,12 @@
  */
 package com.oracle.truffle.r.engine.repl;
 
-import com.oracle.truffle.r.engine.TruffleRLanguageDebug;
 import com.oracle.truffle.tools.debug.shell.server.REPLServer;
 
 public final class RREPL {
     public static void main(String[] args) {
 
-        // Cheating for the prototype: start from SL, rather than from the client.
-        final REPLServer server = new REPLServer("application/x-r", new TruffleRLanguageDebug.RVisualizer());
+        final REPLServer server = new REPLServer("application/x-r");
         server.start();
     }
 
