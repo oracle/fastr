61f5b65b24ac80629fb2d7de03d4e14c26072008
Lukas Stadler <lukas.stadler@oracle.com>
2016-03-01 09:57:01 +0100

    additional tests

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_setdiff.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_setdiff.java
@@ -4,7 +4,7 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
  * Copyright (c) 2014, Purdue University
- * Copyright (c) 2014, 2015, Oracle and/or its affiliates
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates
  *
  * All rights reserved.
  */
@@ -21,6 +21,11 @@ public class TestBuiltin_setdiff extends TestBase {
     @Test
     public void testsetdiff1() {
         assertEval("argv <- structure(list(x = c('bibtex', 'tex'), y = '.svn'), .Names = c('x',     'y'));do.call('setdiff', argv)");
+    }
+
+    @Test
+    public void setdiff() {
+        assertEval("x <- c('a', 'b', 'x'); y <- c('a', 'y', 'z', 'x'); setdiff(x, y)");
     }
 
 }
