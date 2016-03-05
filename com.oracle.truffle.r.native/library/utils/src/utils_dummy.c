7688e8f6e25e76fc1e27fa5dc92cdfb087c741e8
Mick Jordan <mick.jordan@oracle.com>
2016-03-04 16:06:29 -0800

    remove overriding init.c files in default packages

---
+++ com.oracle.truffle.r.native/library/utils/src/utils_dummy.c
@@ -0,0 +1,60 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 2005-12   The R Core Team.
+ *
+ *  This program is free software{ unimplemented_utils(); return NULL; } you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation{ unimplemented_utils(); return NULL; } either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY{ unimplemented_utils(); return NULL; } without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program{ unimplemented_utils(); return NULL; } if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
+
+#include <Rinternals.h>
+
+static void unimplemented_utils() {
+	error("unimplemented utils .External");
+}
+
+SEXP objectSize(SEXP s) { unimplemented_utils(); return NULL; }
+SEXP unzip(SEXP args) { unimplemented_utils(); return NULL; }
+SEXP Rprof(SEXP args) { unimplemented_utils(); return NULL; }
+SEXP Rprofmem(SEXP args) { unimplemented_utils(); return NULL; }
+
+SEXP countfields(SEXP args) { unimplemented_utils(); return NULL; }
+SEXP flushconsole(void) { unimplemented_utils(); return NULL; }
+SEXP menu(SEXP args) { unimplemented_utils(); return NULL; }
+SEXP readtablehead(SEXP args) { unimplemented_utils(); return NULL; }
+SEXP typeconvert(SEXP call, SEXP op, SEXP args, SEXP env) { unimplemented_utils(); return NULL; }
+SEXP writetable(SEXP call, SEXP op, SEXP args, SEXP env) { unimplemented_utils(); return NULL; }
+
+SEXP crc64(SEXP in) { unimplemented_utils(); return NULL; }
+SEXP nsl(SEXP hostname) { unimplemented_utils(); return NULL; }
+SEXP download(SEXP args) { unimplemented_utils(); return NULL; }
+
+SEXP sockconnect(SEXP sport, SEXP shost) { unimplemented_utils(); return NULL; }
+SEXP sockread(SEXP sport, SEXP smaxlen) { unimplemented_utils(); return NULL; }
+SEXP sockclose(SEXP sport) { unimplemented_utils(); return NULL; }
+SEXP sockopen(SEXP sport) { unimplemented_utils(); return NULL; }
+SEXP socklisten(SEXP sport) { unimplemented_utils(); return NULL; }
+SEXP sockwrite(SEXP sport, SEXP sstring) { unimplemented_utils(); return NULL; }
+
+SEXP addhistory(SEXP call, SEXP op, SEXP args, SEXP rho) { unimplemented_utils(); return NULL; }
+SEXP loadhistory(SEXP call, SEXP op, SEXP args, SEXP rho) { unimplemented_utils(); return NULL; }
+SEXP savehistory(SEXP call, SEXP op, SEXP args, SEXP rho) { unimplemented_utils(); return NULL; }
+SEXP dataentry(SEXP call, SEXP op, SEXP args, SEXP rho) { unimplemented_utils(); return NULL; }
+SEXP dataviewer(SEXP call, SEXP op, SEXP args, SEXP rho) { unimplemented_utils(); return NULL; }
+SEXP edit(SEXP call, SEXP op, SEXP args, SEXP rho) { unimplemented_utils(); return NULL; }
+SEXP fileedit(SEXP call, SEXP op, SEXP args, SEXP rho) { unimplemented_utils(); return NULL; }
+SEXP selectlist(SEXP call, SEXP op, SEXP args, SEXP rho) { unimplemented_utils(); return NULL; }
+
+SEXP processevents(void) { unimplemented_utils(); return NULL; }
+
+SEXP octsize(SEXP s) { unimplemented_utils(); return NULL; }
