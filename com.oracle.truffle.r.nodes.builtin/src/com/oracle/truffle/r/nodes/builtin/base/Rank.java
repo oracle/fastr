3fd7d1205f67e1c2917738a49ff44b9ad82372c5
Mick Jordan <mick.jordan@oracle.com>
2016-03-09 16:35:07 -0800

    copyright fix

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Rank.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Rank.java
@@ -3,8 +3,9 @@
  * Version 2. You may review the terms of this license at
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Copyright (c) 1995, 1996  Robert Gentleman and Ross Ihaka
- * Copyright (c) 1997-2013,  The R Core Team
+ * Copyright (c) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
+ * Copyright (c) 1995-2014, The R Core Team
+ * Copyright (c) 2002-2008, The R Foundation
  * Copyright (c) 2016, Oracle and/or its affiliates
  *
  * All rights reserved.
