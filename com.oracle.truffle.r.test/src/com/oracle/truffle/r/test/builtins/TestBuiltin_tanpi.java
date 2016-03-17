e55231e7a53fbb8e845a5af6b9f24f481578efaf
Zbynek Slajchrt <zbynek.slajchrt@oracle.com>
2016-03-17 14:54:01 +0100

    Copyrights updated

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_tanpi.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_tanpi.java
@@ -4,7 +4,7 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
  * Copyright (c) 2012-2014, Purdue University
- * Copyright (c) 2013, 2016, Oracle and/or its affiliates
+ * Copyright (c) 2016, Oracle and/or its affiliates
  *
  * All rights reserved.
  */
