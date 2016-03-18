c203025af7ad8964ef89586bf303c92f1ca7268d
Mick Jordan <mick.jordan@oracle.com>
2016-03-18 08:50:33 -0700

    remove graal-core import

--- mx.fastr/mx_fastr.py
+++ mx.fastr/mx_fastr.py
@@ -574,16 +574,21 @@ def installcran(args):
     script = join(cran_test, 'r', 'install.cran.packages.R')
     return rscript([script] + args)
 
-def load_optional_suite(name, rev):
-    hg_base = mx.get_env('MX_HG_BASE')
-    urlinfos = None if hg_base is None else [mx.SuiteImportURLInfo(join(hg_base, name), 'hg', mx.vc_system('hg'))]
+def load_optional_suite(name, rev, kind='hg', build=True, url=None):
+    if not url:
+        hg_base = mx.get_env('MX_' + kind.upper() + '_BASE')
+        if hg_base is None:
+            url = None
+        else:
+            url = join(hg_base, name)
+    urlinfos = None if url is None else [mx.SuiteImportURLInfo(url, kind, mx.vc_system(kind))]
     opt_suite = _fastr_suite.import_suite(name, version=rev, urlinfos=urlinfos)
-    if opt_suite:
+    if opt_suite and build:
         mx.build_suite(opt_suite)
     return opt_suite
 
 _r_apptests_rev = '2f363c204f713520ea1d881af71bac8962a82c72'
-_r_benchmarks_rev = '0b4f36819086323aebce7a2d7bc62949ff90950b'
+_r_benchmarks_rev = 'ecca4e50c2782a227468274a10d515d4d641e4ad'
 
 def mx_post_parse_cmd_line(opts):
     # load optional suites, r_apptests first so r_benchmarks can find it
