81a31082c140e538aa56c6f3419090aab10eba67
Zbynek Slajchrt <zbynek.slajchrt@oracle.com>
2016-03-22 16:19:47 +0100

    Sign builtin implemented

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Sign.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Sign.java
@@ -23,15 +23,51 @@
 package com.oracle.truffle.r.nodes.builtin.base;
 
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.r.nodes.binary.BoxPrimitiveNode;
+import com.oracle.truffle.r.nodes.binary.BoxPrimitiveNodeGen;
 import com.oracle.truffle.r.nodes.builtin.RBuiltinNode;
+import com.oracle.truffle.r.nodes.unary.UnaryArithmeticNode;
+import com.oracle.truffle.r.nodes.unary.UnaryArithmeticNodeGen;
 import com.oracle.truffle.r.runtime.RBuiltin;
 import com.oracle.truffle.r.runtime.RBuiltinKind;
 import com.oracle.truffle.r.runtime.RError;
+import com.oracle.truffle.r.runtime.RRuntime;
+import com.oracle.truffle.r.runtime.RType;
+import com.oracle.truffle.r.runtime.data.RComplex;
+import com.oracle.truffle.r.runtime.ops.UnaryArithmetic;
 
-@RBuiltin(name = "sign", kind = RBuiltinKind.PRIMITIVE, parameterNames = {})
+@RBuiltin(name = "sign", kind = RBuiltinKind.PRIMITIVE, parameterNames = {"x"})
 public abstract class Sign extends RBuiltinNode {
+
+    @Child private BoxPrimitiveNode boxPrimitive = BoxPrimitiveNodeGen.create();
+    @Child private UnaryArithmeticNode signNode = UnaryArithmeticNodeGen.create(SignArithmetic::new, RType.Logical,
+                        RError.Message.ARGUMENTS_PASSED_0_1, new Object[]{getRBuiltin().name()});
+
     @Specialization
-    protected Object sign(@SuppressWarnings("unused") Object x) {
-        throw RError.nyi(this, "sign");
+    protected Object sign(Object x) {
+        return signNode.execute(boxPrimitive.execute(x));
+    }
+
+    static final class SignArithmetic extends UnaryArithmetic {
+
+            @Override
+            public int op(byte op) {
+                return op == RRuntime.LOGICAL_TRUE ? 1 : 0;
+            }
+
+            @Override
+            public int op(int op) {
+                return op == 0 ? 0 : (op > 0 ? 1 : -1);
+            }
+
+            @Override
+            public double op(double op) {
+                return Math.signum(op);
+            }
+
+            @Override
+            public RComplex op(double re, double im) {
+                throw new UnsupportedOperationException();
+            }
     }
 }
