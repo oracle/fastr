7737d87eaecd514762a829a9e61e19adecfa0329
Adam Welc <adam.welc@oracle.com>
2016-03-22 21:21:32 -0700

    Fixes to S4 slot accesses.

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/S4/TestS4.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/S4/TestS4.java
@@ -42,8 +42,13 @@ public class TestS4 extends TestRBase {
         assertEval("{ c(42)@.Data }");
         assertEval("{ x<-42; `@`(x, \".Data\") }");
         assertEval("{ x<-42; `@`(x, .Data) }");
+        assertEval("{ x<-42; slot(x, \".Data\") }");
+        assertEval("{ setClass(\"foo\", contains=\"numeric\"); x<-new(\"foo\"); res<-x@.Data; removeClass(\"foo\"); res }");
+        assertEval("{ setClass(\"foo\", contains=\"numeric\"); x<-new(\"foo\"); res<-slot(x, \".Data\"); removeClass(\"foo\"); res }");
         assertEval(Output.ContainsError, "{ getClass(\"ClassUnionRepresentation\")@foo }");
         assertEval(Output.ContainsError, "{ c(42)@foo }");
+        assertEval(Output.ContainsError, " { x<-42; attr(x, \"foo\")<-7; x@foo }");
+        assertEval("{ x<-42; attr(x, \"foo\")<-7; slot(x, \"foo\") }");
         assertEval(Output.ContainsError, "{ x<-c(42); class(x)<-\"bar\"; x@foo }");
         assertEval("{ x<-getClass(\"ClassUnionRepresentation\"); slot(x, \"virtual\") }");
         assertEval(Output.ContainsError, "{ x<-getClass(\"ClassUnionRepresentation\"); slot(x, virtual) }");
@@ -59,6 +64,8 @@ public class TestS4 extends TestRBase {
         assertEval("{ x<-getClass(\"ClassUnionRepresentation\"); slot(x, \"virtual\", check=TRUE)<-TRUE; x@virtual }");
         assertEval("{ x<-initialize@valueClass; initialize@valueClass<-\"foo\"; initialize@valueClass<-x }");
 
+        assertEval(Output.ContainsError, "{ x<-function() 42; attr(x, \"foo\")<-7; y@foo<-42 }");
+        assertEval(Output.ContainsError, "{ x<-function() 42; attr(x, \"foo\")<-7; slot(y, \"foo\")<-42 }");
         assertEval(Output.ContainsError, "{ x<-function() 42; attr(x, \"foo\")<-7; y<-asS4(x); y@foo<-42 }");
         assertEval(Output.ContainsError, "{ x<-NULL; `@<-`(x, foo, \"bar\") }");
         assertEval(Output.ContainsError, "{ x<-NULL; x@foo<-\"bar\" }");
