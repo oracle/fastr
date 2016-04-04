61a70b1a6494e3903def37a45cb3533c0826f11e
Mick Jordan <mick.jordan@oracle.com>
2016-04-04 15:46:13 -0700

    add missing file

---
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Strtrim.java
@@ -0,0 +1,80 @@
+/*
+ * This material is distributed under the GNU General Public License
+ * Version 2. You may review the terms of this license at
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright (c) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
+ * Copyright (c) 1995-2014, The R Core Team
+ * Copyright (c) 2002-2008, The R Foundation
+ * Copyright (c) 2016, Oracle and/or its affiliates
+ *
+ * All rights reserved.
+ */
+package com.oracle.truffle.r.nodes.builtin.base;
+
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.r.nodes.builtin.CastBuilder;
+import com.oracle.truffle.r.nodes.builtin.RBuiltinNode;
+import com.oracle.truffle.r.runtime.RBuiltin;
+import com.oracle.truffle.r.runtime.RBuiltinKind;
+import com.oracle.truffle.r.runtime.RError;
+import com.oracle.truffle.r.runtime.RRuntime;
+import com.oracle.truffle.r.runtime.data.RAttributeProfiles;
+import com.oracle.truffle.r.runtime.data.RDataFactory;
+import com.oracle.truffle.r.runtime.data.RStringVector;
+import com.oracle.truffle.r.runtime.data.model.RAbstractIntVector;
+import com.oracle.truffle.r.runtime.data.model.RAbstractStringVector;
+
+@RBuiltin(name = "strtrim", kind = RBuiltinKind.INTERNAL, parameterNames = {"x", "width"})
+public abstract class Strtrim extends RBuiltinNode {
+
+    private RAttributeProfiles attrProfiles = RAttributeProfiles.create();
+
+    @Override
+    protected void createCasts(CastBuilder casts) {
+        casts.toInteger(1);
+    }
+
+    @Specialization
+    protected RStringVector srtrim(RAbstractStringVector x, RAbstractIntVector width) {
+        int len = x.getLength();
+        int nw = width.getLength();
+        if (nw == 0 || nw < len && (len % nw != 0)) {
+            throw RError.error(this, RError.Message.INVALID_ARGUMENT, "width");
+        }
+        for (int i = 0; i < nw; i++) {
+            int widthi = width.getDataAt(i);
+            if (widthi == RRuntime.INT_NA || widthi < 0) {
+                throw RError.error(this, RError.Message.INVALID_ARGUMENT, "width");
+            }
+        }
+        String[] data = new String[len];
+        boolean complete = RDataFactory.COMPLETE_VECTOR;
+        for (int i = 0; i < len; i++) {
+            String element = x.getDataAt(i);
+            if (RRuntime.isNA(element)) {
+                data[i] = element;
+                complete = RDataFactory.INCOMPLETE_VECTOR;
+                continue;
+            }
+            // TODO multibyte character handling
+            int w = width.getDataAt(i % nw);
+            if (w > element.length()) {
+                data[i] = element;
+            } else {
+                data[i] = element.substring(0, w);
+            }
+        }
+        RStringVector result = RDataFactory.createStringVector(data, complete);
+        result.copyAttributesFrom(attrProfiles, x);
+        return result;
+    }
+
+    @SuppressWarnings("unused")
+    @Fallback
+    RStringVector strtrim(Object x, Object width) {
+        throw RError.error(this, RError.Message.REQUIRES_CHAR_VECTOR, "strtrim()");
+    }
+
+}
