33e572a2fa41d34199dde14c5964c8d13dac3819
Lukas Stadler <lukas.stadler@oracle.com>
2016-07-26 13:20:16 +0200

    new RBehavior specification in RBuiltin

--- com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/builtins/RBuiltin.java
+++ com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/builtins/RBuiltin.java
@@ -68,6 +68,18 @@ public @interface RBuiltin {
     RVisibility visibility() default RVisibility.ON;
 
     /**
+     * Determines how calls to a builtin should be dispatched, e.g., whether internal or group
+     * generic dispatch should be used.
+     */
+    RDispatch dispatch() default RDispatch.DEFAULT;
+
+    /**
+     * The behavior defines which conditions can be expected to hold for calls to this builtin,
+     * .e.g., whether repeated calls with the same arguments are expected to return the same result.
+     */
+    RBehavior behavior() default RBehavior.UNKNOWN;
+
+    /**
      * Indicates whether or not function containing a call of the form
      * <code>.Internal(name(...))</code> should trigger a split of the caller at its direct call
      * sites. <code>name</code> indicates the builtin name defined in {@link #name()}.
@@ -75,6 +87,4 @@ public @interface RBuiltin {
     boolean splitCaller() default false;
 
     boolean alwaysSplit() default false;
-
-    RDispatch dispatch() default RDispatch.DEFAULT;
 }
