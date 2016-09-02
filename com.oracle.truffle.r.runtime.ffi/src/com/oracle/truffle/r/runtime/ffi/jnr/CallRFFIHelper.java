7cbefa140491e07442b16e9d0bcbc33186f75ed3
Adam Welc <adam.welc@oracle.com>
2016-09-02 22:06:19 +0200

    Used more appropriate API call to retrive a path from source section.

--- com.oracle.truffle.r.runtime.ffi/src/com/oracle/truffle/r/runtime/ffi/jnr/CallRFFIHelper.java
+++ com.oracle.truffle.r.runtime.ffi/src/com/oracle/truffle/r/runtime/ffi/jnr/CallRFFIHelper.java
@@ -1363,10 +1363,9 @@ public class CallRFFIHelper {
         } else {
             RFunction f = (RFunction) o;
             SourceSection ss = f.getRootNode().getSourceSection();
-            String path = ss.getSource().getPath();
-            // TODO: is it correct to pass "" if path is null?
+            String path = RSource.getPath(ss.getSource());
+            // TODO: is it OK to pass "" if path is null?
             return RSrcref.createLloc(ss, path == null ? "" : path);
-
         }
 
     }
