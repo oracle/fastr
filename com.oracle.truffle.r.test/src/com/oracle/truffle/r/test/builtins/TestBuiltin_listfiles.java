5eb2a7bebaf85fb12327bccd0ac583cddd1f7c2b
Mick Jordan <mick.jordan@oracle.com>
2016-09-09 08:47:11 -0700

    unit tests: remove unstable/redundant listfiles tests

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_listfiles.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_listfiles.java
@@ -18,27 +18,6 @@ import com.oracle.truffle.r.test.TestBase;
 public class TestBuiltin_listfiles extends TestBase {
 
     @Test
-    public void testlistfiles1() {
-        assertEval("argv <- list('.', 'myTst_.*tar\\\\.gz$', FALSE, FALSE, FALSE, FALSE, FALSE, FALSE); .Internal(list.files(argv[[1]], argv[[2]], argv[[3]], argv[[4]], argv[[5]], argv[[6]], argv[[7]], argv[[8]]))");
-    }
-
-    @Test
-    public void testlistfiles2() {
-        assertEval("argv <- list('./myTst/data', NULL, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE); .Internal(list.files(argv[[1]], argv[[2]], argv[[3]], argv[[4]], argv[[5]], argv[[6]], argv[[7]], argv[[8]]))");
-    }
-
-    @Test
-    public void testlistfiles3() {
-        // FastR bug; not recursing in to "."
-        assertEval("argv <- list('.', '^CITATION.*', FALSE, FALSE, TRUE, FALSE, FALSE, FALSE); .Internal(list.files(argv[[1]], argv[[2]], argv[[3]], argv[[4]], argv[[5]], argv[[6]], argv[[7]], argv[[8]]))");
-    }
-
-    @Test
-    public void testlistfiles4() {
-        assertEval("argv <- list('mgcv', NULL, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE); .Internal(list.files(argv[[1]], argv[[2]], argv[[3]], argv[[4]], argv[[5]], argv[[6]], argv[[7]], argv[[8]]))");
-    }
-
-    @Test
     public void testFileListing() {
         assertEval("{ list.files(\"test/r/simple/data/tree1\") }");
         assertEval("{ list.files(\"test/r/simple/data/tree1\", recursive=TRUE) }");
