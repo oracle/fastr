cc9c1d3fc5c08bb8019caa4c365ed16225bdf51c
stepan <stepan.sindelar@oracle.com>
2016-09-21 14:49:48 +0200

    ShortRowNames: converted to castpipelines

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/ShortRowNames.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/ShortRowNames.java
@@ -22,11 +22,12 @@
  */
 package com.oracle.truffle.r.nodes.builtin.base;
 
+import static com.oracle.truffle.r.nodes.builtin.CastBuilder.Predef.gte0;
+import static com.oracle.truffle.r.nodes.builtin.CastBuilder.Predef.lte;
 import static com.oracle.truffle.r.runtime.builtins.RBehavior.PURE;
 import static com.oracle.truffle.r.runtime.builtins.RBuiltinKind.INTERNAL;
 
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.profiles.BranchProfile;
 import com.oracle.truffle.api.profiles.IntValueProfile;
 import com.oracle.truffle.api.profiles.ValueProfile;
@@ -49,42 +50,27 @@ public abstract class ShortRowNames extends RBuiltinNode {
     private final BranchProfile errorProfile = BranchProfile.create();
     private final ValueProfile operandTypeProfile = ValueProfile.createClassProfile();
 
-    public abstract Object executeObject(VirtualFrame frame, Object operand, Object type);
-
     @Override
     protected void createCasts(CastBuilder casts) {
-        casts.toInteger(1);
+        casts.arg("type").asIntegerVector().findFirst().mustBe(gte0().and(lte(2)));
     }
 
     private final IntValueProfile typeProfile = IntValueProfile.createIdentityProfile();
 
     @Specialization
-    protected Object getNames(Object originalOperand, RAbstractIntVector originalType) {
-
-        if (originalType.getLength() == 0) {
-            errorProfile.enter();
-            throw typeError();
-        }
-        int type = typeProfile.profile(originalType.getDataAt(0));
-
-        if (type < 0 || type > 2) {
-            errorProfile.enter();
-            throw typeError();
-        }
-
+    protected Object getNames(Object originalOperand, int originalType) {
         Object operand = operandTypeProfile.profile(originalOperand);
         Object rowNames;
         if (operand instanceof RAbstractContainer) {
             rowNames = ((RAbstractContainer) operand).getRowNames(attrProfiles);
         } else if (operand instanceof REnvironment) {
             rowNames = ((REnvironment) operand).getAttr(attrProfiles, RRuntime.ROWNAMES_ATTR_KEY);
-        } else if (operand instanceof RNull) {
-            return 0;
         } else {
-            errorProfile.enter();
-            throw typeError();
+            // for any other type GnuR returns 0
+            return 0;
         }
 
+        int type = typeProfile.profile(originalType);
         if (type >= 1) {
             int n = calculateN(rowNames);
             rowNames = type == 1 ? n : Math.abs(n);
--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_shortRowNames.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_shortRowNames.java
@@ -127,4 +127,10 @@ public class TestBuiltin_shortRowNames extends TestBase {
     public void testshortRowNames23() {
         assertEval("argv <- list(structure(list(Df = c(NA, 1, 2), Deviance = c(12.2441566485997, 8.44399377410362, 11.9670615295804), AIC = c(73.9421143635373, 72.1419514890412, 77.665019244518)), .Names = c('Df', 'Deviance', 'AIC'), row.names = c('<none>', 'Temp', 'Soft'), class = c('anova', 'data.frame'), heading = c('Single term additions', '\\nModel:', 'cbind(X, M) ~ M.user')), 2L); .Internal(shortRowNames(argv[[1]], argv[[2]]))");
     }
+
+    @Test
+    public void testArgCasts() {
+        assertEval(".Internal(shortRowNames(42, -2))");
+        assertEval(".Internal(shortRowNames(42, '1'))");
+    }
 }
