c1fe9ada968671fbc957f527902fa38626d89b83
stepan <stepan.sindelar@oracle.com>
2017-01-11 15:14:32 +0100

    Stats: port external C_BinDist

---
+++ com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/stats/BinDist.java
@@ -0,0 +1,80 @@
+/*
+ * This material is distributed under the GNU General Public License
+ * Version 2. You may review the terms of this license at
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright (c) 1996-2012, The R Core Team
+ * Copyright (c) 2005, The R Foundation
+ * Copyright (c) 2017, Oracle and/or its affiliates
+ *
+ * All rights reserved.
+ */
+/* Acknowledgement from the original header:
+ *  "HACKED" to allow weights by Adrian Baddeley
+ *  Changes indicated by 'AB'
+ */
+package com.oracle.truffle.r.library.stats;
+
+import static com.oracle.truffle.r.nodes.builtin.CastBuilder.Predef.gt0;
+import static com.oracle.truffle.r.nodes.builtin.CastBuilder.Predef.intNA;
+import static com.oracle.truffle.r.runtime.RError.NO_CALLER;
+
+import java.util.Arrays;
+
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.r.nodes.builtin.CastBuilder;
+import com.oracle.truffle.r.nodes.builtin.RExternalBuiltinNode;
+import com.oracle.truffle.r.runtime.RError.Message;
+import com.oracle.truffle.r.runtime.RRuntime;
+import com.oracle.truffle.r.runtime.data.RDataFactory;
+import com.oracle.truffle.r.runtime.data.RDoubleVector;
+import com.oracle.truffle.r.runtime.data.model.RAbstractDoubleVector;
+
+/**
+ * Implements the C_BinDist external.
+ */
+public abstract class BinDist extends RExternalBuiltinNode.Arg5 {
+    public static BinDist create() {
+        return BinDistNodeGen.create();
+    }
+
+    @Override
+    protected void createCasts(CastBuilder casts) {
+        casts.arg(0).asDoubleVector();
+        casts.arg(1).asDoubleVector();
+        casts.arg(2).asDoubleVector().findFirst();
+        casts.arg(3).asDoubleVector().findFirst();
+        casts.arg(4).asIntegerVector().findFirst().mustBe(gt0().and(intNA().not()), NO_CALLER, Message.INVALID_ARGUMENT, "n");
+    }
+
+    @Specialization
+    RDoubleVector bindist(RAbstractDoubleVector x, RAbstractDoubleVector w, double xlo, double xhi, int n) {
+        int ixmin = 0;
+        int ixmax = n - 2;
+        double xdelta = (xhi - xlo) / (n - 1);
+        double[] result = new double[2 * n];
+        Arrays.fill(result, 0);
+        int wLength = w.getLength();
+
+        for (int i = 0; i < x.getLength(); i++) {
+            if (RRuntime.isFinite(x.getDataAt(i))) {
+                double xpos = (x.getDataAt(i) - xlo) / xdelta;
+                if (!Double.isFinite(xpos)) {
+                    continue;
+                }
+                int ix = (int) Math.floor(xpos);
+                double fx = xpos - ix;
+                double wi = w.getDataAt(i % wLength);
+                if (ixmin <= ix && ix <= ixmax) {
+                    result[ix] += (1 - fx) * wi;
+                    result[ix + 1] += fx * wi;
+                } else if (ix == -1) {
+                    result[0] += fx * wi;
+                } else if (ix == ixmax + 1) {
+                    result[ix] += (1 - fx) * wi;
+                }
+            }
+        }
+        return RDataFactory.createDoubleVector(result, RDataFactory.COMPLETE_VECTOR);
+    }
+}
---
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/library/stats/TestExternal_BinDist.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.r.test.library.stats;
+
+import org.junit.Test;
+
+import com.oracle.truffle.r.test.TestBase;
+
+public class TestExternal_BinDist extends TestBase {
+    @Test
+    public void testBinDist() {
+        assertEval(".Call(stats:::C_BinDist, c(1,2,3), c(4,5,6), 0, 3, 5)");
+        assertEval(".Call(stats:::C_BinDist, c('1','2','3'), c(4,5,6), 0, c(3, 4), c('5', '8'))");
+        assertEval(".Call(stats:::C_BinDist, c(0,0,0), c(1,2,3), 1, 10, 10)");
+        assertEval(".Call(stats:::C_BinDist, c(2.2,4,3), 5:7, -1, 5, 8)");
+    }
+
+    @Test
+    public void testBinDistWrongArgs() {
+        assertEval(Output.IgnoreWarningContext, ".Call(stats:::C_BinDist, 0, 0, 'string', 3, 5)");
+        assertEval(".Call(stats:::C_BinDist, c(1,2,3), c(4,5,6), 0, 3, c(NA, 3L))");
+        assertEval(".Call(stats:::C_BinDist, c(1,2,3), c(4,5,6), 0, 3, -5L)");
+    }
+}
