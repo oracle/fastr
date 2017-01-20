3be15b7e191e55455215708334c01b22f635f148
stepan <stepan.sindelar@oracle.com>
2017-01-20 14:28:27 +0100

    Unify double/int sequences: they can have length==0

--- com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/data/RDoubleSequence.java
+++ com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/data/RDoubleSequence.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,7 +36,7 @@ public final class RDoubleSequence extends RSequence implements RAbstractDoubleV
 
     RDoubleSequence(double start, double stride, int length) {
         super(length);
-        assert length > 0;
+        assert length >= 0;
         this.start = start;
         this.stride = stride;
     }
