44e12ce4fd6597273cb00a159e6a8614b5670142
Lukas Stadler <lukas.stadler@oracle.com>
2017-04-12 13:23:19 +0200

    introduce SHARED cluster

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/parallel/R/forkcluster_overrides.R
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/parallel/R/forkcluster_overrides.R
@@ -11,22 +11,35 @@
 
 ## Derived from snow and parallel packages
 
-eval(expression(
+eval(expression({
+
+# overwritten functions:
+
+makeCluster <- function (spec, type = getClusterOption("type"), ...) {
+    switch(type,
+        PSOCK = makePSOCKcluster(spec, ...),
+        FORK = makeForkCluster(spec, ...),
+        SOCK = snow::makeSOCKcluster(spec, ...),
+        MPI = snow::makeMPIcluster(spec, ...),
+        NWS = snow::makeNWScluster(spec, ...),
+        SHARED = makeSHAREDcluster(spec, ...), # this line was added
+        stop("unknown cluster type"))
+}
+
+# added functions:
+
 closeNode.SHAREDnode <- function(node) {
     .fastr.channel.close(node$channel)
-}), asNamespace("parallel"))
+}
 
-eval(expression(
 sendData.SHAREDnode <- function(node, data) {
     .fastr.channel.send(node$channel, data)
-}), asNamespace("parallel"))
+}
 
-eval(expression(
 recvData.SHAREDnode <- function(node) {
     .fastr.channel.receive(node$channel)
-}), asNamespace("parallel"))
+}
 
-eval(expression(
 recvOneData.SHAREDcluster <- function(cl) {
 	channel_ids = lapply(cl, function(l) l[["channel"]])
     res <- .fastr.channel.select(channel_ids)
@@ -35,39 +48,43 @@ recvOneData.SHAREDcluster <- function(cl) {
 	indexes = lapply(cl, function(l, id) if (identical(l[["channel"]], id)) id else as.integer(NA), id=selected_id)
 	node_ind = which(as.double(indexes)==as.double(selected_id))
 	list(node = node_ind, value = res[[2]])
-}), asNamespace("parallel"))
+}
 
-eval(expression(
-fastr.newSHAREDnodes <- function(nnodes, debug, options = defaultClusterOptions)
-{
+newSHAREDnodes <- function(nnodes, debug, options = defaultClusterOptions) {
 	context_code <- vector("character", nnodes)
 	contexts <- vector("integer", nnodes)
 	channels <- vector("integer", nnodes)
+	outfile <- getClusterOption("outfile", options)
 	for (i in 1:nnodes) {
 		# generate unique values for channel keys (addition factor is chosen based on how snow generates port numbers)
 		port <- as.integer(parallel:::getClusterOption("port", options) + i * 1000)
-		script <- file.path(R.home(), "com.oracle.truffle.r.native", "library", "parallel", "RSHAREDnode.R")
-
-    	context_code[[i]] <- paste0("commandArgs<-function() c('--args', 'PORT=", port, "'); source('", script, "')")
-		if (isTRUE(debug)) cat(sprintf("Starting context: %d with code %s\n", i, context_code[[i]]))
+		
+		startup <- substitute(local({
+            makeSHAREDmaster <- function(key) {
+                channel <- .fastr.channel.get(as.integer(key))
+                structure(list(channel=channel), class = "SHAREDnode")
+            }
+            parallel:::sinkWorkerOutput(OUTFILE)
+            parallel:::slaveLoop(makeSHAREDmaster(PORT))
+        }), list(OUTFILE=outfile, PORT=port))
+		
+        context_code[[i]] <- paste0(deparse(startup), collapse="\n")
+        if (isTRUE(debug)) cat(sprintf("Starting context: %d with code %s\n", i, context_code[[i]]))
 
 		## Need to return a list here, in the same form as the
 		## "cluster" data structure.
     	channels[[i]] <- .fastr.channel.create(port)
 		if (isTRUE(debug)) cat(sprintf("Context %d started!\n", i))
 	}
-    contexts <- .fastr.context.spawn(context_code, nnodes)
+    contexts <- .fastr.context.spawn(context_code)
     cl <- vector("list", nnodes)
 	for (i in 1:nnodes) {
 		cl[[i]] <- structure(list(channel = channels[[i]], context=contexts[[i]], rank = i), class = "SHAREDnode")
 	}
 	cl
-	
-}), asNamespace("parallel"))
+}
 
-makeForkClusterExpr <- expression({
-makeForkCluster <- function(nnodes = getOption("mc.cores", 2L), options = defaultClusterOptions, ...)
-{
+makeSHAREDcluster <- function(nnodes = getOption("mc.cores", 2L), options = defaultClusterOptions, ...) {
     nnodes <- as.integer(nnodes)
     if(is.na(nnodes) || nnodes < 1L) stop("'nnodes' must be >= 1")
     .check_ncores(nnodes)
@@ -76,25 +93,22 @@ makeForkCluster <- function(nnodes = getOption("mc.cores", 2L), options = defaul
 	# Add the "debug" option defaulted to FALSE, if the user didn't specify
 	# If the user gives TRUE, print extra stuff during cluster setup
 	debug <- FALSE
-	tryCatch(
-		debug <- parallel:::getClusterOption("debug", options),
-		error = function(e) { }
-	)
-	options <- parallel:::addClusterOptions(options, list(debug = debug))
+	if (exists("debug", envir=options, inherits=FALSE)) {
+		debug <- parallel:::getClusterOption("debug", options)
+	} else {
+		options <- parallel:::addClusterOptions(options, list(debug = debug))
+	}
 	
-    cl <- fastr.newSHAREDnodes(nnodes, debug = debug, options=options)
+    cl <- newSHAREDnodes(nnodes, debug = debug, options=options)
 	class(cl) <- c("SHAREDcluster", "cluster")
 	cl
-}; environment(makeForkCluster)<-asNamespace("parallel")})
-eval(makeForkClusterExpr, asNamespace("parallel"))
-# seems like we don't need these anymore, but let's make sure
-#eval(makeForkClusterExpr, as.environment("package:parallel"))
+}
 
-
-eval(expression(
 stopCluster.SHAREDcluster <- function(cl) {
     for (n in cl) {
         parallel:::postNode(n, "DONE")
         .fastr.context.join(n$context)
     }
+}
+
 }), asNamespace("parallel"))
