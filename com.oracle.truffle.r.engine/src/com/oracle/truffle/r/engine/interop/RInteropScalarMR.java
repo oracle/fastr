5801e65cc964983c74543a63ccc12c401ff00f51
Tomas Stupka <tomas.stupka@oracle.com>
2017-05-19 17:37:59 +0200

    message resolution for RInteropScalar-s

---
+++ com.oracle.truffle.r.engine/src/com/oracle/truffle/r/engine/interop/RInteropScalarMR.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.r.engine.interop;
+
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.interop.CanResolve;
+import com.oracle.truffle.api.interop.MessageResolution;
+import com.oracle.truffle.api.interop.Resolve;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.profiles.ValueProfile;
+import com.oracle.truffle.r.runtime.RInternalError;
+import com.oracle.truffle.r.runtime.data.RInteropScalar;
+
+@MessageResolution(receiverType = RInteropScalar.class)
+public class RInteropScalarMR {
+
+    @Resolve(message = "IS_BOXED")
+    public abstract static class RInteropScalarIsBoxedNode extends Node {
+        protected Object access(@SuppressWarnings("unused") RInteropScalar receiver) {
+            return true;
+        }
+    }
+
+    @Resolve(message = "UNBOX")
+    public abstract static class RInteropScalarUnboxNode extends Node {
+        private final ValueProfile classProfile = ValueProfile.createClassProfile();
+
+        protected Object access(RInteropScalar receiver) {
+            RInteropScalar t = classProfile.profile(receiver);
+            if (t instanceof RInteropScalar.RInteropByte) {
+                return ((RInteropScalar.RInteropByte) t).getValue();
+            } else if (t instanceof RInteropScalar.RInteropChar) {
+                return ((RInteropScalar.RInteropChar) t).getValue();
+            } else if (t instanceof RInteropScalar.RInteropFloat) {
+                return ((RInteropScalar.RInteropFloat) t).getValue();
+            } else if (t instanceof RInteropScalar.RInteropLong) {
+                return ((RInteropScalar.RInteropLong) t).getValue();
+            } else if (t instanceof RInteropScalar.RInteropShort) {
+                return ((RInteropScalar.RInteropShort) t).getValue();
+            }
+            throw RInternalError.unimplemented("missing RInteropScalar : " + receiver.getClass().getName());
+        }
+    }
+
+    @Resolve(message = "HAS_SIZE")
+    public abstract static class RInteropScalarHasSizeNode extends Node {
+        protected Object access(@SuppressWarnings("unused") RInteropScalar receiver) {
+            return false;
+        }
+    }
+
+    @Resolve(message = "IS_NULL")
+    public abstract static class RInteropScalarIsNullNode extends Node {
+        protected Object access(@SuppressWarnings("unused") RInteropScalar receiver) {
+            return false;
+        }
+    }
+
+    @CanResolve
+    public abstract static class RInteropScalarCheck extends Node {
+        protected static boolean test(TruffleObject receiver) {
+            return receiver instanceof RInteropScalar;
+        }
+    }
+}
---
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/engine/interop/RInteropScalarMRTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.r.test.engine.interop;
+
+import com.oracle.truffle.api.interop.ForeignAccess;
+import com.oracle.truffle.api.interop.Message;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnknownIdentifierException;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.interop.UnsupportedTypeException;
+import com.oracle.truffle.api.source.Source;
+import com.oracle.truffle.api.vm.PolyglotEngine;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RInteropScalarMRTest {
+
+    @Test
+    public void testRInteroptScalar() throws UnsupportedMessageException, UnknownIdentifierException, UnsupportedTypeException {
+        testRIS("toByte", "" + Byte.MAX_VALUE, Byte.class);
+        testRIS("toChar", "'a'", Character.class);
+        testRIS("toFloat", "" + Float.MAX_VALUE, Float.class);
+        testRIS("toLong", "" + Long.MAX_VALUE, Long.class);
+        testRIS("toShort", "" + Short.MAX_VALUE, Short.class);
+    }
+
+    private void testRIS(String toInteropScalarBuiltin, String value, Class<?> unboxedType) throws UnsupportedMessageException {
+        TruffleObject l = createRInteroptScalarTO(toInteropScalarBuiltin, value);
+
+        assertFalse(ForeignAccess.sendIsNull(Message.IS_NULL.createNode(), l));
+        assertFalse(ForeignAccess.sendHasSize(Message.HAS_SIZE.createNode(), l));
+
+        assertTrue(ForeignAccess.sendIsBoxed(Message.IS_BOXED.createNode(), l));
+        Object ub = ForeignAccess.sendUnbox(Message.UNBOX.createNode(), l);
+        assertEquals(unboxedType, ub.getClass());
+    }
+
+    private TruffleObject createRInteroptScalarTO(String toInteropScalarBuiltin, String value) {
+        PolyglotEngine engine = PolyglotEngine.newBuilder().build();
+        Source src = Source.newBuilder(".fastr.interop." + toInteropScalarBuiltin + "(" + value + ")").mimeType("text/x-r").name("test.R").build();
+        PolyglotEngine.Value result = engine.eval(src);
+        return result.as(TruffleObject.class);
+    }
+
+}
