aad82a4f5c6b8952b86cdacf3737b526cb938252
Florian Angerer <florian.angerer@oracle.com>
2017-09-14 13:45:42 +0200

    Refined tests for builin 'dump'.

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_dump.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_dump.java
@@ -17,25 +17,26 @@ import com.oracle.truffle.r.test.TestBase;
 // Checkstyle: stop line length check
 public class TestBuiltin_dump extends TestBase {
 
-    private static final String TEMPLATE = "{ %s; .Internal(dump(%s, stdout(), environment(), .deparseOpts('all'), TRUE))}";
+    private static final String TEMPLATE = "{ %s; .Internal(dump(%s, stdout(), environment(), .deparseOpts(\"all\"), %s))}";
 
     @Test
     public void testDumpFunctions() {
-        assertEval(String.format(TEMPLATE, "fun <- function() print('Hello, World!')", "'fun'"));
-        assertEval(String.format(TEMPLATE, "foo <- function() cat('Hello'); bar <- function() cat('World')", "c('foo', 'bar')"));
+        assertEval(Output.IgnoreWhitespace, String.format(TEMPLATE, "fun <- function() print(\"Hello, World!\")", "\"fun\"", "TRUE"));
+        assertEval(Output.IgnoreWhitespace, String.format(TEMPLATE, "foo <- function() cat(\"Hello\"); bar <- function() cat(\"World\")", "c(\"foo\", \"bar\")", "TRUE"));
     }
 
     @Test
     public void testDumpData() {
-        assertEval(String.format(TEMPLATE, "x <- 10", "'x'"));
-        assertEval(String.format(TEMPLATE, "x <- list(a=10,b=20)", "'x'"));
-        assertEval(String.format(TEMPLATE, "x <- c(10,20,25)", "'x'"));
-        assertEval(String.format(TEMPLATE, "x <- 1:10", "'x'"));
+        assertEval(Output.IgnoreWhitespace, String.format(TEMPLATE, "x <- 10", "\"x\"", "TRUE"));
+        assertEval(Output.IgnoreWhitespace, String.format(TEMPLATE, "x <- list(a=10,b=20)", "\"x\"", "TRUE"));
+        assertEval(Output.IgnoreWhitespace, String.format(TEMPLATE, "x <- c(10,20,25)", "\"x\"", "TRUE"));
+        assertEval(Output.IgnoreWhitespace, String.format(TEMPLATE, "x <- 1:10", "\"x\"", "TRUE"));
     }
 
     @Test
     public void testDumpLanguage() {
-        assertEval(String.format(TEMPLATE, "x <- quote(2 + 3)", "'x'"));
+        assertEval(Output.IgnoreWhitespace, String.format(TEMPLATE, "x <- 2 + 3", "\"x\"", "FALSE"));
+        assertEval(Output.IgnoreWhitespace, String.format(TEMPLATE, "x <- quote(2 + 3)", "\"x\"", "FALSE"));
     }
 
 }
