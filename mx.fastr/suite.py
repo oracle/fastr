8e42eba81b52bc780081ccfec984a068bc3af56a
Florian Angerer <florian.angerer@oracle.com>
2017-09-18 10:50:28 +0200

    Updated Truffle version.

--- mx.fastr/suite.py
+++ mx.fastr/suite.py
@@ -29,7 +29,7 @@ suite = {
             {
                "name" : "truffle",
                "subdir" : True,
-               "version" : "73196584fec60b58ff8d70fd1c3d29cc2634d557",
+               "version" : "91fb53bf8827298be8541caca3f22b096bbad0ea",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
