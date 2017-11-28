2c2de34de689b67b6402b5cf5e26de8092ce583f
Lukas Stadler <lukas.stadler@oracle.com>
2017-11-28 17:13:17 +0100

    fix overly conservative assertion in BinaryMapNode

--- com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/primitive/BinaryMapNode.java
+++ com.oracle.truffle.r.nodes/src/com/oracle/truffle/r/nodes/primitive/BinaryMapNode.java
@@ -496,7 +496,7 @@ abstract class VectorMapBinaryInternalNode extends RBaseNode {
                     @Cached("createCountingProfile()") LoopConditionProfile leftProfile,
                     @Cached("createCountingProfile()") LoopConditionProfile rightProfile,
                     @Cached("createBinaryProfile()") ConditionProfile smallRemainderProfile) {
-        assert result != right;
+        assert result != right || rightLength == leftLength;
         leftProfile.profileCounted(leftLength);
         rightProfile.profileCounted(rightLength);
         while (leftProfile.inject(leftIter.getIndex() + 1 < leftLength)) {
@@ -527,7 +527,7 @@ abstract class VectorMapBinaryInternalNode extends RBaseNode {
                     @Cached("createCountingProfile()") LoopConditionProfile leftProfile,
                     @Cached("createCountingProfile()") LoopConditionProfile rightProfile,
                     @Cached("createBinaryProfile()") ConditionProfile smallRemainderProfile) {
-        assert result != left;
+        assert result != left || rightLength == leftLength;
         leftProfile.profileCounted(leftLength);
         rightProfile.profileCounted(rightLength);
         while (rightProfile.inject(rightIter.getIndex() + 1 < rightLength)) {
