8231533f25d43a0514c803a7da491acabcd84d75
Tomas Stupka <tomas.stupka@oracle.com>
2018-02-16 14:03:14 +0100

    a simple combine on REnvironment, RExpression, RFunction, RLanguage, RSymbol, RS4Object has to return a list

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_c.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_c.java
@@ -4,7 +4,7 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
  * Copyright (c) 2012-2014, Purdue University
- * Copyright (c) 2013, 2017, Oracle and/or its affiliates
+ * Copyright (c) 2013, 2018, Oracle and/or its affiliates
  *
  * All rights reserved.
  */
@@ -541,6 +541,19 @@ public class TestBuiltin_c extends TestBase {
         assertEval("{ x<-c(a=42); y<-c(b=7); z<-c(x,y); w<-names(z); w[[1]]<-\"c\"; z }");
 
         assertEval("typeof(c(substitute(graphics::par), list(as.symbol('a'))))");
+
+        assertEval("c(x~a)");
+        assertEval("c(x~a, x~b)");
+        assertEval("{ f <- function(){}; l <- c(f); print(is.list(l)); identical(l[[1]], f) }");
+        assertEval("{ f <- function(){}; l <- c(f, f); print(is.list(l)); identical(l[[1]], f) }");
+        assertEval("c(environment())");
+        assertEval("c(environment(), environment())");
+        assertEval("c(as.expression(1))");
+        assertEval("c(as.expression(1), as.expression(1))");
+        assertEval("c(as.symbol(1))");
+        assertEval("c(as.symbol(1), as.symbol(1))");
+        assertEval("{ setClass('foo', representation(bar = 'ANY')); c(new('foo', bar=1)) }");
+        assertEval("{ setClass('foo', representation(bar = 'ANY')); c(new('foo', bar=1), new('foo', bar=1)) }");
     }
 
     @Test
