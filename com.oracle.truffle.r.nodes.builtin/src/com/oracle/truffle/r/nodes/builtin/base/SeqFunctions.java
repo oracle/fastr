8a5fc1f7cfe8d60a3eff87b83b1bb549929e844c
Lukas Stadler <lukas.stadler@oracle.com>
2018-05-09 13:28:02 +0200

    fix typo in license (which stems from original R)

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/SeqFunctions.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/SeqFunctions.java
@@ -4,7 +4,7 @@
  * Copyright (c) 2013, 2018, Oracle and/or its affiliates
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Pulic License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
