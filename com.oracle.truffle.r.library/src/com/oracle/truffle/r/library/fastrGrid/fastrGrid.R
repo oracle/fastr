4571ee67b585f677b932375add66c4cb90e84019
Lukas Stadler <lukas.stadler@oracle.com>
2018-05-09 10:14:07 +0200

    reinstate the original, more generic, license for overrides files

--- com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/fastrGrid/fastrGrid.R
+++ com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/fastrGrid/fastrGrid.R
@@ -1,13 +1,17 @@
 #
-# This material is distributed under the GNU General Public License
-# Version 2. You may review the terms of this license at
-# http://www.gnu.org/licenses/gpl-2.0.html
-#
 # Copyright (C) 2001-3 Paul Murrell
 # Copyright (c) 1998-2015, The R Core Team
-# Copyright (c) 2017, Oracle and/or its affiliates
+# Copyright (c) 2017, 2018, Oracle and/or its affiliates
 #
-# All rights reserved.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 
 # This file contains internal R helper functions that are invoked from grid externals implementations.
