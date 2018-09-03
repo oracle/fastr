17ae3f6ef11804ce2ba94da8da76e3e9b73a1453
paley <paley.li@oracle.com>
2018-09-03 12:05:15 +0200

    test correction

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_sortfastpass.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_sortfastpass.java
@@ -19,7 +19,7 @@ public class TestBuiltin_sortfastpass extends TestBase {
         assertEval(".Internal(sorted_fpass(1.5:5.5, FALSE, FALSE))");
         assertEval(".Internal(sorted_fpass(seq(1.5,5.5,0.5), FALSE, FALSE))");
         assertEval(".Internal(sorted_fpass(paste('hello',1:10,'.'), FALSE, FALSE))");
-        assertEval("argv <- list(1:10, FALSE, FALSE); argv2 <- argv + 1; .Internal(sorted_fpass(argv2[[1]], argv2[[2]], argv2[[3]]))");
-        assertEval("argv <- list(c(1,2,3,4,5,6,7,8), FALSE, FALSE); argv2 <- argv + 1; .Internal(sorted_fpass(argv2[[1]], argv2[[2]], argv2[[3]]))");
+        assertEval("argv <- list(1:10, FALSE, FALSE); argv2 <- argv[[1]] + 1; .Internal(sorted_fpass(argv2[[1]], argv2[[2]], argv2[[3]]))");
+        assertEval("argv <- list(c(1,2,3,4,5,6,7,8), FALSE, FALSE); argv2 <- argv[[1]] + 1; .Internal(sorted_fpass(argv2[[1]], argv2[[2]], argv2[[3]]))");
     }
 }
