336b4c615d78cc3f569c0069590b0267cb1b7d61
stepan <stepan.sindelar@oracle.com>
2018-09-05 16:22:56 +0200

    Add quantmod to gated pkgs

--- com.oracle.truffle.r.test.packages/r/install.packages.R
+++ com.oracle.truffle.r.test.packages/r/install.packages.R
@@ -151,7 +151,8 @@ default.packages <- c("R", "base", "grid", "splines", "utils",
 # if this configuration gets too complex or updated too often, we can move it to separate config file
 ignore.suggests <- list(
 	rstudioapi = c('*'), # rstudioapi executes almost no real tests, it is mostly just test of install & load
-	glmnet = c('knitr')  # probably used for vignettes only
+	glmnet = c('knitr'),  # probably used for vignettes only
+    quantmod = c('*'), # probably not necessary, the tests output does not contain any 'library', 'require' or 'load' calls
 )
 
 choice.depends <- function(pkg, choice=c("direct","suggests")) {
