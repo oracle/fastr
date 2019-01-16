9f4abbaaf0a7e9fbe42e19f16109974afda33cb9
Miloslav Metelka <miloslav.metelka@oracle.com>
2019-01-16 22:20:59 +0100

    Add complex support into signif() and fix NA handling problem.

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Signif.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Signif.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -35,6 +35,7 @@ import java.math.MathContext;
 import java.math.RoundingMode;
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.profiles.BranchProfile;
 import com.oracle.truffle.api.profiles.ConditionProfile;
@@ -42,17 +43,17 @@ import com.oracle.truffle.r.nodes.attributes.CopyAttributesNode;
 import com.oracle.truffle.r.nodes.attributes.CopyAttributesNodeGen;
 import com.oracle.truffle.r.nodes.builtin.RBuiltinNode;
 import com.oracle.truffle.r.runtime.RError;
-import com.oracle.truffle.r.runtime.RInternalError;
 import com.oracle.truffle.r.runtime.RRuntime;
-import com.oracle.truffle.r.runtime.Utils;
 import com.oracle.truffle.r.runtime.builtins.RBuiltin;
+import com.oracle.truffle.r.runtime.data.RComplexVector;
 import com.oracle.truffle.r.runtime.data.RDataFactory;
 import com.oracle.truffle.r.runtime.data.RDoubleVector;
 import com.oracle.truffle.r.runtime.data.RMissing;
 import com.oracle.truffle.r.runtime.data.model.RAbstractComplexVector;
 import com.oracle.truffle.r.runtime.data.model.RAbstractDoubleVector;
 import com.oracle.truffle.r.runtime.data.model.RAbstractIntVector;
-import com.oracle.truffle.r.runtime.ops.na.NACheck;
+import com.oracle.truffle.r.runtime.data.nodes.VectorAccess;
+import com.oracle.truffle.r.runtime.data.nodes.VectorAccess.SequentialIterator;
 
 @RBuiltin(name = "signif", kind = PRIMITIVE, parameterNames = {"x", "digits"}, dispatch = MATH_GROUP_GENERIC, behavior = PURE)
 public abstract class Signif extends RBuiltinNode.Arg2 {
@@ -63,10 +64,6 @@ public abstract class Signif extends RBuiltinNode.Arg2 {
     }
 
     @Child private CopyAttributesNode copyAttributes = CopyAttributesNodeGen.create(true);
-    private final NACheck naCheck = NACheck.create();
-    private final BranchProfile empty = BranchProfile.create();
-    private final BranchProfile identity = BranchProfile.create();
-    private final ConditionProfile infProfile = ConditionProfile.createBinaryProfile();
 
     static {
         Casts casts = new Casts(Signif.class);
@@ -79,60 +76,125 @@ public abstract class Signif extends RBuiltinNode.Arg2 {
     }
 
     // TODO: consider porting signif implementation from GNU R
-
-    @Specialization
-    protected RAbstractDoubleVector signif(RAbstractDoubleVector x, RAbstractIntVector digitsVec) {
-        int xLength = x.getLength();
-        if (x.getLength() == 0) {
-            empty.enter();
+    @Specialization(guards = {"xAccess.supports(x)", "digitsAccess.supports(digits)"}, limit = "getVectorAccessCacheSize()")
+    protected RAbstractDoubleVector signifDouble(RAbstractDoubleVector x, RAbstractIntVector digits,
+                    @Cached("x.access()") VectorAccess xAccess,
+                    @Cached("digits.access()") VectorAccess digitsAccess,
+                    @Cached("create()") BranchProfile emptyProfile,
+                    @Cached("create()") BranchProfile identityProfile,
+                    @Cached("createBinaryProfile()") ConditionProfile infProfile) {
+        SequentialIterator xIter = xAccess.access(x);
+        SequentialIterator digitsIter = digitsAccess.access(digits);
+        int xLength = xAccess.getLength(xIter);
+        if (xLength == 0) {
+            emptyProfile.enter();
             return RDataFactory.createEmptyDoubleVector();
         }
-        int digitsVecLength = digitsVec.getLength();
-        int maxLength = Math.max(xLength, digitsVecLength);
-        double[] data = new double[maxLength];
-        int xInd = 0;
-        int digitsVecInd = 0;
-        for (int i = 0; i < maxLength; i++) {
-            int digits = digitsVec.getDataAt(digitsVecInd);
-            if (digits > 22) {
-                identity.enter();
-                data[i] = x.getDataAt(xInd);
+        int digitsLength = digitsAccess.getLength(digitsIter); // always >0
+        int resultLength = Math.max(xLength, digitsLength);
+        double[] resultData = new double[resultLength];
+        boolean resultComplete = true;
+        for (int i = 0; i < resultLength; i++) {
+            xAccess.nextWithWrap(xIter);
+            digitsAccess.nextWithWrap(digitsIter);
+            double res;
+            if (digitsAccess.isNA(digitsIter) || xAccess.isNA(xIter)) {
+                res = RRuntime.DOUBLE_NA;
+                resultComplete = false;
             } else {
-                if (digits <= 0) {
-                    digits = 1;
-                }
-                naCheck.enable(!(x.isComplete() && digitsVec.isComplete()));
-                double val = x.getDataAt(xInd);
-                double result;
-                if (naCheck.check(val)) {
-                    result = RRuntime.DOUBLE_NA;
-                } else if (naCheck.check(digits)) {
-                    result = RRuntime.DOUBLE_NA;
+                int digitCount = digitsAccess.getInt(digitsIter);
+                if (digitCount > 22) {
+                    identityProfile.enter();
+                    res = xAccess.getDouble(xIter);
                 } else {
+                    if (digitCount <= 0) {
+                        digitCount = 1;
+                    }
+                    double val = xAccess.getDouble(xIter);
                     if (infProfile.profile(Double.isInfinite(val))) {
-                        result = Double.POSITIVE_INFINITY;
+                        res = Double.POSITIVE_INFINITY;
                     } else {
-                        result = bigIntegerSignif(digits, val);
+                        res = bigIntegerSignif(digitCount, val);
                     }
                 }
-                data[i] = result;
             }
-            xInd = Utils.incMod(xInd, xLength);
-            digitsVecInd = Utils.incMod(digitsVecInd, digitsVecLength);
+            resultData[i] = res;
         }
-        RDoubleVector ret = RDataFactory.createDoubleVector(data, naCheck.neverSeenNA());
-        ret = (RDoubleVector) copyAttributes.execute(ret, x, xLength, digitsVec, digitsVecLength);
-        return ret;
+        RDoubleVector resultVec = RDataFactory.createDoubleVector(resultData, resultComplete);
+        resultVec = (RDoubleVector) copyAttributes.execute(resultVec, x, xLength, digits, digitsLength);
+        return resultVec;
     }
 
-    @SuppressWarnings("unused")
-    @Specialization
-    protected RAbstractComplexVector signif(RAbstractComplexVector x, RAbstractIntVector digitsVec) {
-        // TODO: implement for complex numbers but I am not sure GNU R gets it right:
-        // > signif(42.1234-7.1234i, 1)
-        // [1] 40-10i
+    @Specialization(replaces = "signifDouble")
+    protected RAbstractDoubleVector signifDoubleGeneric(RAbstractDoubleVector x, RAbstractIntVector digits) {
+        return signifDouble(x, digits, x.slowPathAccess(), digits.slowPathAccess(),
+                        BranchProfile.create(), BranchProfile.create(), ConditionProfile.createBinaryProfile());
+    }
 
-        throw RInternalError.unimplemented();
+    @Specialization(guards = {"xAccess.supports(x)", "digitsAccess.supports(digits)"}, limit = "getVectorAccessCacheSize()")
+    protected RAbstractComplexVector signifComplex(RAbstractComplexVector x, RAbstractIntVector digits,
+                    @Cached("x.access()") VectorAccess xAccess,
+                    @Cached("digits.access()") VectorAccess digitsAccess,
+                    @Cached("create()") BranchProfile emptyProfile,
+                    @Cached("create()") BranchProfile identityProfile,
+                    @Cached("createBinaryProfile()") ConditionProfile infRProfile,
+                    @Cached("createBinaryProfile()") ConditionProfile infIProfile) {
+        SequentialIterator xIter = xAccess.access(x);
+        SequentialIterator digitsIter = digitsAccess.access(digits);
+        int xLength = xAccess.getLength(xIter);
+        if (xLength == 0) {
+            emptyProfile.enter();
+            return RDataFactory.createEmptyComplexVector();
+        }
+        int digitsLength = digitsAccess.getLength(digitsIter); // always >0
+        int resultLength = Math.max(xLength, digitsLength);
+        double[] resultData = new double[resultLength << 1];
+        boolean resultComplete = true;
+        for (int i = 0; i < resultData.length;) {
+            xAccess.nextWithWrap(xIter);
+            digitsAccess.nextWithWrap(digitsIter);
+            double resR;
+            double resI;
+            if (digitsAccess.isNA(digitsIter) || xAccess.isNA(xIter)) {
+                resR = RRuntime.DOUBLE_NA;
+                resI = RRuntime.DOUBLE_NA;
+                resultComplete = false;
+            } else {
+                int digitCount = digitsAccess.getInt(digitsIter);
+                if (digitCount > 22) {
+                    identityProfile.enter();
+                    resR = xAccess.getComplexR(xIter);
+                    resI = xAccess.getComplexI(xIter);
+                } else {
+                    if (digitCount <= 0) {
+                        digitCount = 1;
+                    }
+                    double valR = xAccess.getComplexR(xIter);
+                    double valI = xAccess.getComplexI(xIter);
+                    if (infRProfile.profile(Double.isInfinite(valR))) {
+                        resR = Double.POSITIVE_INFINITY;
+                    } else {
+                        resR = bigIntegerSignif(digitCount, valR);
+                    }
+                    if (infIProfile.profile(Double.isInfinite(valI))) {
+                        resI = Double.POSITIVE_INFINITY;
+                    } else {
+                        resI = bigIntegerSignif(digitCount, valI);
+                    }
+                }
+            }
+            resultData[i++] = resR;
+            resultData[i++] = resI;
+        }
+        RComplexVector resultVec = RDataFactory.createComplexVector(resultData, resultComplete);
+        resultVec = (RComplexVector) copyAttributes.execute(resultVec, x, xLength, digits, digitsLength);
+        return resultVec;
+    }
+
+    @Specialization(replaces = "signifComplex")
+    protected RAbstractComplexVector signifComplexGeneric(RAbstractComplexVector x, RAbstractIntVector digits) {
+        return signifComplex(x, digits, x.slowPathAccess(), digits.slowPathAccess(),
+                        BranchProfile.create(), BranchProfile.create(), ConditionProfile.createBinaryProfile(), ConditionProfile.createBinaryProfile());
     }
 
     @TruffleBoundary
