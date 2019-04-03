94957f6a948daaf1361277a7fde75dd31a1d62eb
Tomas Stupka <tomas.stupka@oracle.com>
2019-04-03 21:12:24 +0200

    more cases in gzcon.R test

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/library/base/connections/R/gzcon.R
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/library/base/connections/R/gzcon.R
@@ -2,8 +2,10 @@ gzconTestList <- list(1,2,3)
 tmpf <- tempfile()
 save(gzconTestList, file=tmpf) 
 gzconTestList <- NULL
+u <- file(tmpf, open="rb")
+load(u)
+print(gzconTestList)
+gzconTestList <- NULL
 u <- url(paste0("file:///", tmpf), open="rb")
 load(u)
 print(gzconTestList)
-# wrong warning ctx
-#u <- gzcon(u)
