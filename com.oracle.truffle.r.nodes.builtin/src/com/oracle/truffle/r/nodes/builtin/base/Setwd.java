2fe2870b8c350e1a76dae61e3d1c4ef9661c6c09
Florian Angerer <florian.angerer@oracle.com>
2019-04-10 14:38:55 +0200

    Do never return 'null' in 'getwd' / 'setwd'.

--- com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Setwd.java
+++ com.oracle.truffle.r.nodes.builtin/src/com/oracle/truffle/r/nodes/builtin/base/Setwd.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -35,6 +35,7 @@ import com.oracle.truffle.r.nodes.builtin.RBuiltinNode;
 import com.oracle.truffle.r.runtime.RError;
 import com.oracle.truffle.r.runtime.Utils;
 import com.oracle.truffle.r.runtime.builtins.RBuiltin;
+import com.oracle.truffle.r.runtime.data.RNull;
 import com.oracle.truffle.r.runtime.ffi.BaseRFFI;
 import com.oracle.truffle.r.runtime.ffi.RFFIFactory;
 
@@ -60,7 +61,7 @@ public abstract class Setwd extends RBuiltinNode.Arg1 {
         } else {
             String nwdAbs = getwdNode.execute();
             Utils.updateCurwd(nwdAbs);
-            return owd;
+            return owd != null ? owd : RNull.instance;
         }
     }
 }
