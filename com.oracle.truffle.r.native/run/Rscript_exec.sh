4063ef3029c35cc9c37886dcf46cdcfefe08bc05
Zbynek Slajchrt <zbynek.slajchrt@oracle.com>
2019-07-25 08:24:27 +0200

    Using rpath option instead of LD_LIBRARY_PATH

--- com.oracle.truffle.r.native/run/Rscript_exec.sh
+++ com.oracle.truffle.r.native/run/Rscript_exec.sh
@@ -42,7 +42,4 @@ else
     mx=`which mx`
 fi
 
-LIB="$PRIMARY_PATH/lib"
-export LD_LIBRARY_PATH="$LIB:$LD_LIBRARY_PATH"
-
 exec $mx --no-warning --primary-suite-path $PRIMARY_PATH $MX_R_GLOBAL_ARGS Rscript $FASTR_INTERNAL_ARGS $MX_R_CMD_ARGS "$@"
