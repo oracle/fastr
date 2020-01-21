046952c4eba733ebfb1e9c8e4c46811ef5ca0b98
stepan <stepan.sindelar@oracle.com>
2020-01-21 15:14:18 +0100

    Fix: unwrap parent when checking for RootBodyTag

--- com.oracle.truffle.r.engine/src/com/oracle/truffle/r/engine/RRuntimeASTAccessImpl.java
+++ com.oracle.truffle.r.engine/src/com/oracle/truffle/r/engine/RRuntimeASTAccessImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -299,7 +299,7 @@ class RRuntimeASTAccessImpl implements RRuntimeASTAccess {
             // The only child of the RootBodyNode that is instrumentable should be the body.
             // Any other child of RootBodyNode should not be instrumentable, e.g. SaveArgumentsNode,
             // etc.
-            return node.getParent() instanceof RootBodyNode;
+            return RASTUtils.unwrapParent(node) instanceof RootBodyNode;
         }
         if (node instanceof RootBodyNode) {
             return (tag == RootTag.class);
