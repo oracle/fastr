583e92f0bdb84f6bb61031c1eb2572e271978a11
Pavel Marek <pavel.marek@oracle.com>
2020-03-13 12:39:39 +0100

    Unittests: get rid of try catch

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/altrep/AltRepInterfaceTests.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/altrep/AltRepInterfaceTests.java
@@ -1,30 +1,13 @@
 package com.oracle.truffle.r.test.altrep;
 
 import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.interop.TruffleObject;
-import com.oracle.truffle.api.library.ExportLibrary;
-import com.oracle.truffle.api.library.ExportMessage;
-import com.oracle.truffle.r.runtime.context.AltRepContext;
-import com.oracle.truffle.r.runtime.data.NativeDataAccess;
-import com.oracle.truffle.r.runtime.data.RBaseObject;
-import com.oracle.truffle.r.runtime.data.RDataFactory;
 import com.oracle.truffle.r.runtime.data.RIntVector;
-import com.oracle.truffle.r.runtime.data.RNull;
-import com.oracle.truffle.r.runtime.data.altrep.AltIntegerClassDescriptor;
-import com.oracle.truffle.r.runtime.data.RAltIntVectorData;
-import com.oracle.truffle.r.runtime.data.altrep.RAltRepData;
 import com.oracle.truffle.r.runtime.ffi.VectorRFFIWrapper;
-import com.oracle.truffle.r.runtime.ffi.util.NativeMemory;
-import com.oracle.truffle.r.runtime.ffi.util.NativeMemory.ElementType;
 import com.oracle.truffle.r.test.TestBase;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 public class AltRepInterfaceTests extends TestBase {
-    private AltRepContext altRepContext = AltRepContext.newContextState();
-
     @Test
     public void trivialLengthMethodTest() {
         SimpleDescriptorWrapper simpleDescriptorWrapper = new SimpleDescriptorWrapper();
@@ -50,20 +33,15 @@ public class AltRepInterfaceTests extends TestBase {
      * This code snippet does not have to materialize instance, because it has Elt method registered.
      */
     @Test
-    public void readNativeIntData() {
+    public void readNativeIntData() throws Exception {
         SimpleDescriptorWrapper simpleDescriptorWrapper = new SimpleDescriptorWrapper();
         RIntVector altIntVec = simpleDescriptorWrapper.getAltIntVector();
                 //RDataFactory.createAltIntVector(simpleDescriptorWrapper.getDescriptor(), simpleDescriptorWrapper.getAltIntVectorData().getData());
 
         // int *data = INTEGER(instance);
         VectorRFFIWrapper rffiWrapper = VectorRFFIWrapper.get(altIntVec);
-        Object elem = null;
-        try {
-            // int elem = data[0];
-            elem = InteropLibrary.getFactory().getUncached(rffiWrapper).readArrayElement(rffiWrapper, 0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // int elem = data[0];
+        Object elem = InteropLibrary.getFactory().getUncached(rffiWrapper).readArrayElement(rffiWrapper, 0);;
 
         // altIntVec does not have to be materialized, because it has Elt method registered, therefore code
         // snippet "int elem = data[0]" downcalls into Elt method rather than into Dataptr method.
@@ -77,18 +55,14 @@ public class AltRepInterfaceTests extends TestBase {
      * data[0] = 42;
      */
     @Test
-    public void writeNativeIntData() {
+    public void writeNativeIntData() throws Exception {
         SimpleDescriptorWrapper simpleDescriptorWrapper = new SimpleDescriptorWrapper();
         RIntVector altIntVec = simpleDescriptorWrapper.getAltIntVector();
 
         // int *data = INTEGER(instance);
         VectorRFFIWrapper rffiWrapper = VectorRFFIWrapper.get(altIntVec);
-        try {
-            // data[0] = 42;
-            InteropLibrary.getFactory().getUncached(rffiWrapper).writeArrayElement(rffiWrapper, 0, 42);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // data[0] = 42;
+        InteropLibrary.getFactory().getUncached(rffiWrapper).writeArrayElement(rffiWrapper, 0, 42);
 
         // data[0] = 42 calls into Dataptr method, so the whole vector has to be materialized.
         Assert.assertTrue(altIntVec.isMaterialized());
