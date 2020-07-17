1900c047dc418e4785420f6660747f466a046777
Pavel Marek <pavel.marek@oracle.com>
2020-07-17 12:45:20 +0200

    AltrepDuplicateNode supports all types

--- com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/nodes/altrep/AltrepDuplicateNode.java
+++ com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/nodes/altrep/AltrepDuplicateNode.java
@@ -2,12 +2,17 @@ package com.oracle.truffle.r.runtime.nodes.altrep;
 
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateUncached;
+import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.profiles.ConditionProfile;
+import com.oracle.truffle.r.runtime.RInternalError;
 import com.oracle.truffle.r.runtime.data.RDataFactory;
+import com.oracle.truffle.r.runtime.data.RDoubleArrayVectorData;
+import com.oracle.truffle.r.runtime.data.RDoubleVector;
 import com.oracle.truffle.r.runtime.data.RIntArrayVectorData;
 import com.oracle.truffle.r.runtime.data.RIntVector;
 import com.oracle.truffle.r.runtime.data.altrep.AltrepUtilities;
+import com.oracle.truffle.r.runtime.data.model.RAbstractAtomicVector;
 import com.oracle.truffle.r.runtime.ffi.AltrepRFFI;
 import com.oracle.truffle.r.runtime.ffi.FFIUnwrapNode;
 import com.oracle.truffle.r.runtime.ffi.util.NativeMemory;
@@ -15,40 +20,45 @@ import com.oracle.truffle.r.runtime.ffi.util.NativeMemory.ElementType;
 import com.oracle.truffle.r.runtime.nodes.RBaseNode;
 
 @GenerateUncached
+@ImportStatic(AltrepUtilities.class)
 public abstract class AltrepDuplicateNode extends RBaseNode {
     public abstract Object execute(Object altVec, boolean deep);
 
-    @Specialization(guards = {"hasDuplicateMethod(altIntVec)"})
-    protected Object doAltIntWithDuplicateMethod(RIntVector altIntVec, boolean deep,
-                                              @Cached("createBinaryProfile()") ConditionProfile duplicateReturnsNullProfile,
-                                              @Cached FFIUnwrapNode unwrapNode,
-                                              @Cached AltrepRFFI.DataptrNode dataptrNode,
-                                              @Cached AltrepRFFI.LengthNode lengthNode,
-                                              @Cached AltrepRFFI.DuplicateNode duplicateNode) {
-        assert AltrepUtilities.isAltrep(altIntVec);
-        Object duplicatedObject = duplicateNode.execute(altIntVec, deep);
+    @Specialization(guards = "hasDuplicateMethodRegistered(altrepVec)")
+    protected Object duplicateAltrepWithDuplicateMethod(RAbstractAtomicVector altrepVec, boolean deep,
+                        @Cached ConditionProfile duplicateReturnsNullProfile,
+                        @Cached FFIUnwrapNode unwrapNode,
+                        @Cached AltrepRFFI.DataptrNode dataptrNode,
+                        @Cached AltrepRFFI.LengthNode lengthNode,
+                        @Cached AltrepRFFI.DuplicateNode duplicateNode) {
+        assert altrepVec.isAltRep();
+        Object duplicatedObject = duplicateNode.execute(altrepVec, deep);
         if (duplicateReturnsNullProfile.profile(duplicatedObject == null)) {
-            return doStandardDuplicate(altIntVec, deep, dataptrNode, lengthNode);
+            return doStandardDuplicate(altrepVec, deep, dataptrNode, lengthNode);
         } else {
-            RIntVector duplicatedVector = (RIntVector) unwrapNode.execute(duplicatedObject);
-            // We have to return data, not the whole vector. However, even if the Duplicate method returns the whole vector.
+            RAbstractAtomicVector duplicatedVector = (RAbstractAtomicVector) unwrapNode.execute(duplicatedObject);
+            // We have to return data, not the whole vector.
             return duplicatedVector.getData();
         }
     }
 
-    @Specialization(replaces = {"doAltIntWithDuplicateMethod"})
-    protected Object doStandardDuplicate(RIntVector altIntVec, @SuppressWarnings("unused") boolean deep,
+    @Specialization(replaces = {"duplicateAltrepWithDuplicateMethod"})
+    protected Object doStandardDuplicate(RAbstractAtomicVector altrepVec, @SuppressWarnings("unused") boolean deep,
                                       @Cached AltrepRFFI.DataptrNode dataptrNode,
                                       @Cached AltrepRFFI.LengthNode lengthNode) {
-        assert AltrepUtilities.isAltrep(altIntVec);
-        int length = lengthNode.execute(altIntVec);
-        long dataptrAddr = dataptrNode.execute(altIntVec, false);
-        int[] newData = new int[length];
-        NativeMemory.copyMemory(dataptrAddr, newData, ElementType.INT, length);
-        return new RIntArrayVectorData(newData, RDataFactory.INCOMPLETE_VECTOR);
-    }
-
-    protected static boolean hasDuplicateMethod(RIntVector altIntVec) {
-        return AltrepUtilities.getAltIntDescriptor(altIntVec).isDuplicateMethodRegistered();
+        assert AltrepUtilities.isAltrep(altrepVec);
+        int length = lengthNode.execute(altrepVec);
+        long dataptrAddr = dataptrNode.execute(altrepVec, false);
+        if (altrepVec instanceof RIntVector) {
+            int[] newData = new int[length];
+            NativeMemory.copyMemory(dataptrAddr, newData, ElementType.INT, length);
+            return new RIntArrayVectorData(newData, RDataFactory.INCOMPLETE_VECTOR);
+        } else if (altrepVec instanceof RDoubleVector) {
+            double[] newData = new double[length];
+            NativeMemory.copyMemory(dataptrAddr, newData, ElementType.DOUBLE, length);
+            return new RDoubleArrayVectorData(newData, RDataFactory.INCOMPLETE_VECTOR);
+        } else {
+            throw RInternalError.unimplemented();
+        }
     }
 }
