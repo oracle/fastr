2afca31a6c56723562799a4bd562d483ab0c6697
Pavel Marek <pavel.marek@oracle.com>
2020-07-17 15:08:40 +0200

    RAltStringVectorData extends RAltrepData

--- com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/data/RAltStringVectorData.java
+++ com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/data/RAltStringVectorData.java
@@ -3,31 +3,24 @@ package com.oracle.truffle.r.runtime.data;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Cached.Exclusive;
 import com.oracle.truffle.api.dsl.Cached.Shared;
-import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
-import com.oracle.truffle.r.runtime.RInternalError;
 import com.oracle.truffle.r.runtime.RType;
-import com.oracle.truffle.r.runtime.data.VectorDataLibrary.RandomAccessIterator;
 import com.oracle.truffle.r.runtime.data.VectorDataLibrary.RandomAccessWriteIterator;
 import com.oracle.truffle.r.runtime.data.VectorDataLibrary.SeqIterator;
 import com.oracle.truffle.r.runtime.data.VectorDataLibrary.SeqWriteIterator;
 import com.oracle.truffle.r.runtime.data.altrep.AltStringClassDescriptor;
 import com.oracle.truffle.r.runtime.data.altrep.RAltRepData;
-import com.oracle.truffle.r.runtime.data.model.RAbstractContainer;
 import com.oracle.truffle.r.runtime.ffi.AltrepRFFI;
-import com.oracle.truffle.r.runtime.ops.na.NACheck;
 
 
 @ExportLibrary(VectorDataLibrary.class)
-public class RAltStringVectorData implements TruffleObject, VectorDataWithOwner {
-    private final RAltRepData altrepData;
+public class RAltStringVectorData extends RAltrepVectorData {
     private final AltStringClassDescriptor descriptor;
-    private RStringVector owner;
 
     public RAltStringVectorData(AltStringClassDescriptor descriptor, RAltRepData altRepData) {
+        super(altRepData);
         assert hasDescriptorRegisteredNecessaryMethods(descriptor);
-        this.altrepData = altRepData;
         this.descriptor = descriptor;
     }
 
@@ -38,65 +31,14 @@ public class RAltStringVectorData implements TruffleObject, VectorDataWithOwner 
                 && descriptor.isSetEltMethodRegistered();
     }
 
-    @Override
-    public void setOwner(RAbstractContainer owner) {
-        this.owner = (RStringVector) owner;
-    }
-
-    public RAltRepData getAltrepData() {
-        return altrepData;
-    }
-
     public AltStringClassDescriptor getDescriptor() {
         return descriptor;
     }
 
-    @ExportMessage
-    public int getLength(@Shared("lengthNode") @Cached AltrepRFFI.LengthNode lengthNode) {
-        return lengthNode.execute(owner);
-    }
-
+    @Override
     @ExportMessage
     public RType getType() {
         return RType.Character;
-    }
-
-    @ExportMessage
-    public NACheck getNACheck() {
-        return NACheck.getDisabled();
-    }
-
-    @ExportMessage
-    public RStringArrayVectorData materialize(@Shared("eltNode") @Cached AltrepRFFI.EltNode eltNode,
-                                              @Shared("lengthNode") @Cached AltrepRFFI.LengthNode lengthNode) {
-        return new RStringArrayVectorData(getStringDataCopy(eltNode, lengthNode), RDataFactory.INCOMPLETE_VECTOR);
-    }
-
-    @ExportMessage
-    public Object copy(boolean deep) {
-        throw RInternalError.unimplemented("copy");
-    }
-
-    // Access to the elements:
-
-    @ExportMessage
-    public SeqIterator iterator(@Shared("lengthNode") @Cached AltrepRFFI.LengthNode lengthNode) {
-        return new SeqIterator(null, getLength(lengthNode));
-    }
-
-    @ExportMessage
-    public boolean nextImpl(SeqIterator it, boolean loopCondition) {
-        throw RInternalError.unimplemented("nextImpl");
-    }
-
-    @ExportMessage
-    public void nextWithWrap(SeqIterator it) {
-        throw RInternalError.unimplemented("nextWithWrap");
-    }
-
-    @ExportMessage
-    public RandomAccessIterator randomAccessIterator() {
-        return new RandomAccessIterator(null);
     }
 
     @ExportMessage
@@ -122,33 +64,21 @@ public class RAltStringVectorData implements TruffleObject, VectorDataWithOwner 
         return (String) eltNode.execute(owner, it.getIndex());
     }
 
-    // Write access to the elements:
-
-    @ExportMessage
-    public SeqWriteIterator writeIterator(@Shared("lengthNode") @Cached AltrepRFFI.LengthNode lengthNode) {
-        return new SeqWriteIterator(null, getLength(lengthNode));
-    }
-
-    @ExportMessage
-    public RandomAccessWriteIterator randomAccessWriteIterator() {
-        return new RandomAccessWriteIterator(null);
-    }
-
     @ExportMessage
     public void setStringAt(int index, String value,
                             @Exclusive @Cached AltrepRFFI.SetEltNode setEltNode) {
-        setEltNode.execute(owner, index, value);
+        setEltNode.execute((RStringVector) owner, index, value);
     }
 
     @ExportMessage
     public void setNextString(SeqWriteIterator it, String value,
                               @Exclusive @Cached AltrepRFFI.SetEltNode setEltNode) {
-            setEltNode.execute(owner, it.getIndex(), value);
+            setEltNode.execute((RStringVector) owner, it.getIndex(), value);
     }
 
     @ExportMessage
     public void setString(@SuppressWarnings("unused") RandomAccessWriteIterator it, int index, String value,
                           @Exclusive @Cached AltrepRFFI.SetEltNode setEltNode) {
-        setEltNode.execute(owner, index, value);
+        setEltNode.execute((RStringVector) owner, index, value);
     }
 }
