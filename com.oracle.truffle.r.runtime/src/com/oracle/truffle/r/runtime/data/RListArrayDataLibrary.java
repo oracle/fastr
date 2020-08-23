3a052044ea36f6df7ef4573f2982512938a41c6b
Pavel Marek <pavel.akira.marek@gmail.com>
2020-08-23 14:53:48 +0200

    Implement deep copy message in RListArrayDataLibrary.

--- com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/data/RListArrayDataLibrary.java
+++ com.oracle.truffle.r.runtime/src/com/oracle/truffle/r/runtime/data/RListArrayDataLibrary.java
@@ -24,15 +24,19 @@ package com.oracle.truffle.r.runtime.data;
 
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Cached.Shared;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
 import com.oracle.truffle.api.profiles.ConditionProfile;
 import com.oracle.truffle.api.profiles.LoopConditionProfile;
+import com.oracle.truffle.r.runtime.RInternalError;
 import com.oracle.truffle.r.runtime.RType;
 import com.oracle.truffle.r.runtime.data.VectorDataLibrary.RandomAccessIterator;
 import com.oracle.truffle.r.runtime.data.VectorDataLibrary.RandomAccessWriteIterator;
 import com.oracle.truffle.r.runtime.data.VectorDataLibrary.SeqIterator;
 import com.oracle.truffle.r.runtime.data.VectorDataLibrary.SeqWriteIterator;
+import com.oracle.truffle.r.runtime.data.model.RAbstractContainer;
 import com.oracle.truffle.r.runtime.ops.na.NACheck;
 
 @ExportLibrary(value = VectorDataLibrary.class, receiverType = Object[].class)
@@ -63,8 +67,46 @@ public class RListArrayDataLibrary {
     }
 
     @ExportMessage
-    public static Object[] copy(Object[] receiver, @SuppressWarnings("unused") boolean deep) {
-        return getListDataCopy(receiver);
+    public static class Copy {
+        @Specialization(guards = "!deep")
+        public static Object[] copyShallow(Object[] receiver, @SuppressWarnings("unused") boolean deep) {
+            return getListDataCopy(receiver);
+        }
+
+        @Specialization(guards = {"deep", "length(receiver) <= 3"})
+        public static Object[] copyWithDispatchedLibrary(Object[] receiver, @SuppressWarnings("unused") boolean deep,
+                                                         @CachedLibrary(limit = "3") AbstractContainerLibrary containerLib) {
+            Object[] result = new Object[receiver.length];
+            for (int i = 0; i < receiver.length; i++) {
+                result[i] = deepCopyElement(receiver[i], containerLib);
+            }
+            return result;
+        }
+
+        @Specialization(guards = {"deep", "length(receiver) > 3"})
+        public static Object[] copyUncached(Object[] receiver, @SuppressWarnings("unused") boolean deep) {
+            AbstractContainerLibrary containerLib = AbstractContainerLibrary.getFactory().getUncached();
+            Object[] result = new Object[receiver.length];
+            for (int i = 0; i < receiver.length; i++) {
+                result[i] = deepCopyElement(receiver[i], containerLib);
+            }
+            return result;
+        }
+
+        private static Object deepCopyElement(Object element, AbstractContainerLibrary containerLib) {
+            if (element instanceof RAbstractContainer) {
+                return containerLib.duplicate(element, true);
+            } else if (element instanceof RSharingAttributeStorage) {
+                // Handle more general and slower case for RS4Object or RFunction.
+                return ((RSharingAttributeStorage) element).deepCopy();
+            } else {
+                return element;
+            }
+        }
+
+        protected static int length(Object[] objects) {
+            return objects.length;
+        }
     }
 
     @ExportMessage
