da54dfbbf4e9365fd903c5403ca38b544d289ded
peterz <peterz@disroot.org>
2021-01-26 10:40:08 +0300

    Fixed #include and CFLAGS for libf2c

--- com.oracle.truffle.r.native/f2c/libf2c-patch/fpu_control.h
+++ com.oracle.truffle.r.native/f2c/libf2c-patch/fpu_control.h
@@ -23,5 +23,5 @@
 
 /* Used to avoid compiler error on musl due to missing header file */
 #if !defined(__linux__) || defined(__GLIBC__)
-#include_next "fpu_control.h"
+#include <fpu_control.h>
 #endif
