23bab2f327d3a01c072e2f114d7478c92ec16a88
stepan <stepan.sindelar@oracle.com>
2021-03-16 11:46:26 +0100

    Adapt JavaGD to the new API

--- com.oracle.truffle.r.native/fficall/src/JavaGD/jGDtalk.c
+++ com.oracle.truffle.r.native/fficall/src/JavaGD/jGDtalk.c
@@ -628,6 +628,23 @@ static void newJavaGD_Raster(unsigned int *raster, int w, int h,
 	gdRaster(xd->gdId, raster, w, h, x, y, width, height, rot, interpolate);
 }
 
+static SEXP newJavaGD_setPattern(SEXP pattern, pDevDesc dd) {
+    return R_NilValue;
+}
+
+static void newJavaGD_releasePattern(SEXP ref, pDevDesc dd) {}
+
+static SEXP newJavaGD_setClipPath(SEXP path, SEXP ref, pDevDesc dd) {
+    return R_NilValue;
+}
+
+static void newJavaGD_releaseClipPath(SEXP ref, pDevDesc dd) {}
+
+static SEXP newJavaGD_setMask(SEXP path, SEXP ref, pDevDesc dd) {
+    return R_NilValue;
+}
+
+static void newJavaGD_releaseMask(SEXP ref, pDevDesc dd) {}
 
 /*-----------------------------------------------------------------------*/
 
@@ -659,6 +676,14 @@ void setupJavaGDfunctions(NewDevDesc *dd) {
     dd->path = newJavaGD_Path;
 #if R_GE_version >= 9
     dd->holdflush = newJavaGD_HoldFlush;
+#if R_GE_version >= 13
+    dd->setPattern      = newJavaGD_setPattern;
+    dd->releasePattern  = newJavaGD_releasePattern;
+    dd->setClipPath     = newJavaGD_setClipPath;
+    dd->releaseClipPath = newJavaGD_releaseClipPath;
+    dd->setMask         = newJavaGD_setMask;
+    dd->releaseMask     = newJavaGD_releaseMask;
+#endif
 #endif
 #endif
 #endif
