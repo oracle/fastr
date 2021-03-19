a4a07dbcc23490e6409cb78f793b617ff2275f80
Pavel Marek <pavel.m.marek@oracle.com>
2021-03-19 15:27:50 +0100

    Remove TestBuiltin_unlink.testExpandParameter.
    It was causing troubles in style gate.

--- com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_unlink.java
+++ com.oracle.truffle.r.test/src/com/oracle/truffle/r/test/builtins/TestBuiltin_unlink.java
@@ -20,52 +20,12 @@
  */
 package com.oracle.truffle.r.test.builtins;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.oracle.truffle.r.test.TestBase;
 
 // Checkstyle: stop line length check
 public class TestBuiltin_unlink extends TestBase {
-    private static final String fileName = "tmp_file77845610.txt";
-    private static final String filePath = "~" + File.separator + fileName;
-    private static final Path fileInHomeDir = Paths.get(System.getProperty("user.home"), fileName);
-    private static final Path tildeDir = Paths.get("~");
-    private static final Path fileInTildeDir = Paths.get("~", fileName);
-
-    @BeforeClass
-    public static void createFiles() {
-        try {
-            if (!Files.exists(tildeDir)) {
-                Files.createDirectory(tildeDir);
-            }
-            Files.createFile(fileInHomeDir);
-            Files.createFile(fileInTildeDir);
-        } catch (IOException e) {
-            // no-op - if creation of files fails, we just ignore the tests that use them.
-        }
-    }
-
-    @AfterClass
-    public static void deleteFiles() {
-        try {
-            Files.deleteIfExists(fileInHomeDir);
-            Files.deleteIfExists(fileInTildeDir);
-            Files.deleteIfExists(tildeDir);
-        } catch (IOException e) {
-            // no-op - we just leave the files there if something wrong occurs.
-        }
-    }
-
     @Test
     public void testunlink1() {
         assertEval(Ignored.SideEffects, "argv <- list('/tmp/RtmptPgrXI/Pkgs', TRUE, FALSE); .Internal(unlink(argv[[1]], argv[[2]], argv[[3]]))");
@@ -94,22 +54,5 @@ public class TestBuiltin_unlink extends TestBase {
         assertEval("{ td <- tempfile(pattern='r-test-unlink'); dir.exists(td); dir.create(td); dir.exists(td); unlink(td, recursive=T); dir.exists(td) }");
         assertEval("{ td <- tempfile(pattern='r-test-unlink'); td <- paste0(td, '/dir1/dir2'); dir.exists(td); dir.create(td, recursive=T); dir.exists(td); unlink(td, recursive=F); dir.exists(td) }");
         assertEval("{ td <- tempfile(pattern='r-test-unlink'); td <- paste0(td, '/dir1/dir2'); dir.exists(td); dir.create(td, recursive=T); dir.exists(td); unlink(td, recursive=T); dir.exists(td) }");
-    }
-
-    /**
-     * Creates a random file in user home directory and in "tilde" directory, i.e., in ~ directory.
-     * Expand parameter to unlink builtin should either expand ~ as user home, or interpret it
-     * literally.
-     */
-    @Test
-    public void testUnlinkExpandParameter() {
-        Assume.assumeTrue(Files.exists(fileInHomeDir));
-        Assume.assumeTrue(Files.exists(fileInTildeDir));
-
-        assertEval(String.format("{ res <- unlink('%s', expand=TRUE); res }", filePath));
-        Assert.assertTrue(!Files.exists(fileInHomeDir) && Files.exists(fileInTildeDir));
-
-        assertEval(String.format("{ res <- unlink('%s', expand=FALSE); res }", filePath));
-        Assert.assertTrue(!Files.exists(fileInHomeDir) && !Files.exists(fileInTildeDir));
     }
 }
