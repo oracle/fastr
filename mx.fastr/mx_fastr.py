378f85900ea0854989e99f90143766e992f97394
Tomas Zezula <tomas.zezula@oracle.com>
2021-04-08 17:37:01 +0200

    [GR-30565] Update ANTLR to 4.9.2.

--- mx.fastr/mx_fastr.py
+++ mx.fastr/mx_fastr.py
@@ -918,7 +918,7 @@ def build_binary_pkgs(args_in, **kwargs):
 
 COPYRIGHT_HEADER_GPL3 = """\
 /*
- * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
