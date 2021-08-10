a80842793df263255e2a11b001dda002ec3e91c8
Pavel Marek <pavel.m.marek@oracle.com>
2021-08-10 12:37:17 +0200

    Fix C_ApproxTest

--- com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/stats/ApproxTest.java
+++ com.oracle.truffle.r.library/src/com/oracle/truffle/r/library/stats/ApproxTest.java
@@ -2,7 +2,7 @@
  * Copyright (c) 1995, 1996, 1997  Robert Gentleman and Ross Ihaka
  * Copyright (c) 1998-2013, The R Core Team
  * Copyright (c) 2003-2015, The R Foundation
- * Copyright (c) 2016, 2020, Oracle and/or its affiliates
+ * Copyright (c) 2016, 2021, Oracle and/or its affiliates
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,7 +27,12 @@ import com.oracle.truffle.r.runtime.RRuntime;
 import com.oracle.truffle.r.runtime.data.RDoubleVector;
 import com.oracle.truffle.r.runtime.data.RNull;
 
-public abstract class ApproxTest extends RExternalBuiltinNode.Arg4 {
+import static com.oracle.truffle.r.nodes.builtin.CastBuilder.Predef.toBoolean;
+
+/**
+ * Transcribed from {@code src/library/stats/src/approx.c:R_approxtest}.
+ */
+public abstract class ApproxTest extends RExternalBuiltinNode.Arg5 {
     public static ApproxTest create() {
         return ApproxTestNodeGen.create();
     }
@@ -36,10 +41,11 @@ public abstract class ApproxTest extends RExternalBuiltinNode.Arg4 {
         Casts casts = new Casts(ApproxTest.class);
         casts.arg(2).asIntegerVector().findFirst();
         casts.arg(3).asDoubleVector().findFirst();
+        casts.arg(4).asLogicalVector().findFirst().map(toBoolean());
     }
 
     @Specialization
-    protected RNull approxtest(RDoubleVector x, RDoubleVector y, int method, double f) {
+    protected RNull approxtest(RDoubleVector x, RDoubleVector y, int method, double f, boolean naRm) {
         int nx = x.getLength();
         switch (method) {
             case 1:
@@ -54,8 +60,16 @@ public abstract class ApproxTest extends RExternalBuiltinNode.Arg4 {
         }
 
         for (int i = 0; i < nx; i++) {
-            if (RRuntime.isNAorNaN(x.getDataAt(i)) || RRuntime.isNAorNaN(y.getDataAt(i))) {
-                throw error(RError.Message.GENERIC, ("approx(): attempted to interpolate NA values"));
+            if (naRm) {
+                // (x,y) should not have any NA's anymore
+                if (RRuntime.isNAorNaN(x.getDataAt(i)) || RRuntime.isNAorNaN(y.getDataAt(i))) {
+                    throw error(RError.Message.GENERIC, ("approx(): attempted to interpolate NA values"));
+                }
+            } else {
+                // na.rm = FALSE ==> at least y may contain NA's
+                if (RRuntime.isNAorNaN(x.getDataAt(i))) {
+                    throw error(RError.Message.GENERIC, ("approx(x,y, .., na.rm=FALSE): NA values in x are not allowed"));
+                }
             }
         }
 
