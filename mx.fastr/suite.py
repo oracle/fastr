eac1c44aacf7971ee634fdc6386332a75956099e
Pavel Marek <pavel.m.marek@oracle.com>
2021-08-12 18:06:21 +0200

    Update Truffle import - add ce-fastr and ee-fastr vms.

--- mx.fastr/suite.py
+++ mx.fastr/suite.py
@@ -9,7 +9,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Sulong
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "4a9158422d34a34fd20b52f73bba6e8ebb48fb9c",
+               "version" : "a94f7d02b95fe8779a6ca4a2f788f90bb636a3c0",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
@@ -20,7 +20,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Truffle
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "4a9158422d34a34fd20b52f73bba6e8ebb48fb9c",
+               "version" : "a94f7d02b95fe8779a6ca4a2f788f90bb636a3c0",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
