dd661e836e70a22409b91cb3bc7d7716a0834dd9
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2022-06-21 19:08:45 +0200

    Update graal imports.

--- mx.fastr/suite.py
+++ mx.fastr/suite.py
@@ -9,7 +9,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Sulong
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "715514619f4a05bf6f423286eaa9a852af3c2150",
+               "version" : "0854fa2567cd3003fe569a6d44c6e9b43d195743",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
@@ -20,7 +20,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Truffle
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "715514619f4a05bf6f423286eaa9a852af3c2150",
+               "version" : "0854fa2567cd3003fe569a6d44c6e9b43d195743",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
