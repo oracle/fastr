65399c63d1ba14472a29f5226d4aa229b51194a0
Roland Schatz <roland.schatz@oracle.com>
2022-12-16 16:46:01 +0100

    Fix implicit SEXP-to-int casts.

--- com.oracle.truffle.r.native/fficall/src/truffle_nfi/rffiutils.h
+++ com.oracle.truffle.r.native/fficall/src/truffle_nfi/rffiutils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -43,6 +43,16 @@
     jmp_buf error_jmpbuf;           \
     pushJmpBuf(&error_jmpbuf);      \
     SEXP result = R_NilValue;       \
+    if (!setjmp(error_jmpbuf)) {    \
+        result = call;              \
+    }                               \
+    popJmpBuf();                    \
+    return result;
+
+#define DO_CALL_INT(call)           \
+    jmp_buf error_jmpbuf;           \
+    pushJmpBuf(&error_jmpbuf);      \
+    int result = 0;                 \
     if (!setjmp(error_jmpbuf)) {    \
         result = call;              \
     }                               \
