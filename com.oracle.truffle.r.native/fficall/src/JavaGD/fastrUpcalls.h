df26b24c787da79b37a9786633f15885231705fc
Roland Schatz <roland.schatz@oracle.com>
2023-01-12 13:57:08 +0100

    Fix deprecated function without prototype.

--- com.oracle.truffle.r.native/fficall/src/JavaGD/fastrUpcalls.h
+++ com.oracle.truffle.r.native/fficall/src/JavaGD/fastrUpcalls.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -37,7 +37,7 @@ void gdClose(int);
 void gdDeactivate(int);
 void gdHold(int);
 void gdFlush(int, int);
-double* gdLocator();
+double* gdLocator(int);
 void gdLine(int, double, double, double, double);
 void gdMode(int, int);
 Rboolean gdOpen(int, const char*, double, double);
