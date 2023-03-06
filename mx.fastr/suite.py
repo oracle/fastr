51442724f1c9150846faca9cde5fc5daba06b11f
stepan <stepan.sindelar@oracle.com>
2023-03-06 13:18:38 +0100

    Update Truffle and Sulong imports

--- mx.fastr/suite.py
+++ mx.fastr/suite.py
@@ -9,7 +9,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Sulong
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "9377addc6deb4a3f7808c7eb32a5d3a409fa86e4",
+               "version" : "bb78ac5b62c932cce530f73984354fff80aed72d",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
@@ -20,7 +20,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Truffle
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "9377addc6deb4a3f7808c7eb32a5d3a409fa86e4",
+               "version" : "bb78ac5b62c932cce530f73984354fff80aed72d",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
