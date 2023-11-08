58e3bf01d0ce585e14ea51f11ce243015de200a3
ol-automation_ww <ol-automation_ww@oracle.com>
2023-11-08 13:25:17 +0000

    Update Truffle and Sulong imports

--- mx.fastr/suite.py
+++ mx.fastr/suite.py
@@ -9,7 +9,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Sulong
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "a22eac89b35be929604b56881e127c200d4c2721",
+               "version" : "c36cabf8e6fa9bb617ed3f4e27aaed6b7adf5768",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
@@ -20,7 +20,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Truffle
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "a22eac89b35be929604b56881e127c200d4c2721",
+               "version" : "c36cabf8e6fa9bb617ed3f4e27aaed6b7adf5768",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
