0593f512a750e9fb4ec9c158891f3c2b23fc351e
Imane Mamri <imane.mamri@oracle.com>
2023-12-18 22:30:07 +0100

    update graal import

--- mx.fastr/suite.py
+++ mx.fastr/suite.py
@@ -9,7 +9,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Sulong
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "933c6602f620a203c80a86d4e9d789bb7d8fb56b",
+               "version" : "8a010c6c2d3886237cceaf12d3f9966503a0e5a6",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
@@ -20,7 +20,7 @@ suite = {
                "subdir" : True,
                # The version must be the same as the version of Truffle
                # TRUFFLE REVISION (note: this is a marker for script that can update this)
-               "version" : "933c6602f620a203c80a86d4e9d789bb7d8fb56b",
+               "version" : "8a010c6c2d3886237cceaf12d3f9966503a0e5a6",
                "urls" : [
                     {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
